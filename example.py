1a2bf702868e4c16f39dc7a5c244528da66d1a81
m4lysh <m4lysh@gmail.com>
2016-07-18 20:11:30 +0200

    modify port

--- example.py
+++ example.py
@@ -599,5 +599,5 @@ def fullmap():
 
 if __name__ == "__main__":
     register_background_thread(initial_registration=True)	
-	port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True, threaded=True)
