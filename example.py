44902e43adebd919d5b3fa6ec43d0e4abb719036
Mohammad Edghaim <m.edghaim@gmail.com>
2016-07-18 18:16:40 -0400

    Credentials cleaned up and rebased

--- example.py
+++ example.py
@@ -38,14 +38,15 @@ API_URL = 'https://pgorelease.nianticlabs.com/plfe/rpc'
 LOGIN_URL = \
     'https://sso.pokemon.com/sso/login?service=https%3A%2F%2Fsso.pokemon.com%2Fsso%2Foauth2.0%2FcallbackAuthorize'
 LOGIN_OAUTH = 'https://sso.pokemon.com/sso/oauth2.0/accessToken'
-PTC_CLIENT_SECRET = \
-    'w8ScCUXJQc6kXKw8FiOhd8Fixzht18Dq3PEVkUCP5ZPxtgyWsbTvWHFLm2wNY0JR'
-ANDROID_ID = '9774d56d682e549c'
-SERVICE = \
-    'audience:server:client_id:848232511240-7so421jotr2609rmqakceuu1luuq0ptb.apps.googleusercontent.com'
-APP = 'com.nianticlabs.pokemongo'
-CLIENT_SIG = '321187995bc7cdc2b5fc91b11a96e2baa8602c62'
-GOOGLEMAPS_KEY = 'AIzaSyAZzeHhs-8JZ7i18MjFuM35dJHq70n3Hx4'
+
+with open('credentials.json') as file:
+	credentials = json.load(file)
+
+PTC_CLIENT_SECRET = credentials.get('ptc_client_secret', None)
+ANDROID_ID = credentials.get('android_id', None)
+SERVICE = credentials.get('service', None)
+CLIENT_SIG = credentials.get('client_sig', None)
+GOOGLEMAPS_KEY = credentials.get('gmaps_key', None)
 
 SESSION = requests.session()
 SESSION.headers.update({'User-Agent': 'Niantic App'})
