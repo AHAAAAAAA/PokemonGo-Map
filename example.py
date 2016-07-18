832101a69517ad5977e6512a4a0759cfdd245de2
Nils <Nils>
2016-07-18 15:51:56 +0200

    changed 'pokemons' list to a map to get rid of duplicates

--- example.py
+++ example.py
@@ -53,11 +53,10 @@ FLOAT_LONG = 0
 deflat, deflng = 0, 0
 default_step = 0.001
 api_endpoint = None
-pokemons = []
+pokemons = {}
 gyms = []
 pokestops = []
 numbertoteam = {0: "Gym", 1: "Mystic", 2: "Valor", 3: "Instinct"} # At least I'm pretty sure that's it. I could be wrong and then I'd be displaying the wrong owner team of gyms.
-
 
 # stuff for in-background search thread
 search_thread = None
@@ -438,24 +437,20 @@ def main():
             pokename = pokemonsJSON[poke.pokemon.PokemonId - 1]['Name']
             if args.ignore:
                 if pokename.lower() in ignore: continue
-            other = LatLng.from_degrees(poke.Latitude, poke.Longitude)
-            diff = other - origin
-            # print(diff)
-            difflat = diff.lat().degrees
-            difflng = diff.lng().degrees
             
             disappear_timestamp = time.time() + poke.TimeTillHiddenMs/1000
-            disappear_time_formatted = datetime.fromtimestamp(disappear_timestamp).strftime("%H:%M:%S")
-            disappears_at = 'disappears at %s' % (disappear_time_formatted)
             
-            pid = str(poke.pokemon.PokemonId)
-            label = (
-                        '<div style=\'position:float; top:0;left:0;\'><small><a href=\'http://www.pokemon.com/us/pokedex/'+pid+'\' target=\'_blank\' title=\'View in Pokedex\'>#'+pid+'</a></small> - <b>'+pokemonsJSON[poke.pokemon.PokemonId - 1]['Name']+'</b></div>'
-                        '<center>'+disappears_at+'</center>'
-                    )
             if args.china:
                 poke.Latitude, poke.Longitude = transform_from_wgs_to_gcj(Location(poke.Latitude, poke.Longitude))
-            pokemons.append([poke.pokemon.PokemonId, label, poke.Latitude, poke.Longitude])
+                               
+            pokemons[poke.SpawnPointId + ":" + poke.TimeTillHiddenMs] = {
+                "lat": poke.Latitude, 
+                "lng": poke.Longitude, 
+                "disappear_time": disappear_timestamp, 
+                "id": poke.pokemon.PokemonId, 
+                "name": pokename
+            }
+
 
         #Scan location math
         if (-steplimit/2 < x <= steplimit/2) and (-steplimit/2 < y <= steplimit/2):
@@ -511,18 +506,25 @@ app = create_app()
 
 @app.route('/')
 def fullmap():
-    pokeMarkers = []
-    for pokemon in pokemons:
-        currLat, currLon = pokemon[-2], pokemon[-1]
-        imgnum = str(pokemon[0]);
-        if len(imgnum) <= 2: imgnum = '0' + imgnum
-        if len(imgnum) <= 2: imgnum = '0' + imgnum
-        pokeMarkers.append(
-            {
-                'icon': 'static/icons/'+str(pokemon[0])+'.png',
-                'lat': currLat,
-                'lng': currLon,
-                'infobox': pokemon[1]
+    pokeMarkers = []    
+    
+    for pokemon_key in pokemons:
+        pokemon = pokemons[pokemon_key]
+        
+        disappear_time_formatted = datetime.fromtimestamp(pokemon['disappear_time']).strftime("%H:%M:%S")
+        disappears_at = 'disappears at %s' % (disappear_time_formatted)
+        
+        label = ( 
+                '<div style=\'position:float; top:0;left:0;\'><small><a href=\'http://www.pokemon.com/us/pokedex/'+str(pokemon['id'])+
+                '\' target=\'_blank\' title=\'View in Pokedex\'>#'+str(pokemon['id'])+'</a></small> - <b>'+ pokemon['name'] +
+                '</b></div><center>'+ disappears_at+'</center>' 
+            )
+                        
+        pokeMarkers.append( {
+                'icon': 'static/icons/%d.png' % pokemon["id"],
+                'lat': pokemon["lat"],
+                'lng': pokemon["lng"],
+                'infobox': label
                 })
     for gym in gyms:
         if gym[0] == 0: color = "white"
