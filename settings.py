a0e7b8957eba59fabbbefb8f10a79e084b51c3e2
borisrozumnuk <borisrozumnuk@gmail.com>
2016-07-18 21:22:31 +0200

    added settings.py file

---
+++ settings.py
@@ -0,0 +1,13 @@
+
+API_URL = 'https://pgorelease.nianticlabs.com/plfe/rpc'
+LOGIN_URL = 'https://sso.pokemon.com/sso/login?service=https%3A%2F%2Fsso.pokemon.com%2Fsso%2Foauth2.0%2FcallbackAuthorize'
+LOGIN_OAUTH = 'https://sso.pokemon.com/sso/oauth2.0/accessToken'
+PTC_CLIENT_SECRET = 'w8ScCUXJQc6kXKw8FiOhd8Fixzht18Dq3PEVkUCP5ZPxtgyWsbTvWHFLm2wNY0JR'
+ANDROID_ID = '9774d56d682e549c'
+SERVICE= 'audience:server:client_id:848232511240-7so421jotr2609rmqakceuu1luuq0ptb.apps.googleusercontent.com'
+APP = 'com.nianticlabs.pokemongo'
+CLIENT_SIG = '321187995bc7cdc2b5fc91b11a96e2baa8602c62'
+GOOGLEMAPS_KEY = "AIzaSyAZzeHhs-8JZ7i18MjFuM35dJHq70n3Hx4"
+
+HOST = "127.0.0.1"
+PORT = 5000
