5b216bf19ce7161c1c23584d434c3d36643ac2b2
Ahmed Osman <ahmed.osman@nordcloud.com>
2016-07-19 13:50:17 +0200

    Cleaned up Pre-Reqs
    * Removed wget and unzip since we're using git now
    * Set versions in pip install so we grab the right versions (reduced startup time of container when we feed requirements.txt to pip)
    * Minor commenting tweaks

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+# If the repo has already been cloned, pull the latest changes
+# Otherwise just clone the repo
 if [ -d "PokemonGo-Map" ]; then
     cd PokemonGo-Map
     git pull
     pip install -r requirements.txt
     python example.py --port 8090 --host 0.0.0.0 "$@"
-  # Control will enter here if $DIRECTORY exists.
+
 else
     git clone https://github.com/AHAAAAAAA/PokemonGo-Map.git
     cd PokemonGo-Map
