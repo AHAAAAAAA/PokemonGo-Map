7133d774bcc2249a93711b46bf86bb840273f4ae
Konstantin Kuklin <konstantin.kuklin@gmail.com>
2016-07-19 03:01:33 +0300

    fixed logs

--- example.py
+++ example.py
@@ -573,7 +573,7 @@ def main():
     origin_lat = FLOAT_LAT
     origin_lon = FLOAT_LONG
     for step in range(steplimit**2):
-        stdout.write("\r[{}] Looping: step {} of {}".format(str(datetime.now()), step, steplimit ** 2))
+        stdout.write("\r[{}] Looping: step {} of {}. ".format(str(datetime.now()), step+1, steplimit ** 2))
 
         # Scan location math
         if -steplimit / 2 < x <= steplimit / 2 and -steplimit / 2 < y \
@@ -589,6 +589,7 @@ def main():
         stdout.write(' (completed: ' + str(
             (step + pos * .25 - .25) / (steplimit**2) * 100) + '%' + ")")
         stdout.flush();
+    print("\n")
 
     if (NEXT_LAT and NEXT_LONG
         and NEXT_LAT != FLOAT_LAT
@@ -599,10 +600,10 @@ def main():
     register_background_thread()
 
 
-print('[+] Searching pokemons for location {} {}}}'.format(FLOAT_LAT, FLOAT_LONG))
 
 def process_step(args, api_endpoint, access_token, profile_response,
                  pokemonsJSON, ignore, only):
+    stdout.write('Location: {} {}'.format(FLOAT_LAT, FLOAT_LONG))
     origin = LatLng.from_degrees(FLOAT_LAT, FLOAT_LONG)
     step_lat = FLOAT_LAT
     step_long = FLOAT_LONG
