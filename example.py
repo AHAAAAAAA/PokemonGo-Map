9d9443a67986c3cdac11bcf38a3d58ad2c0c74a2
Nils <Nils>
2016-07-19 02:34:45 +0200

    changed the label so it displays both disappears-at and disappears-in

--- example.py
+++ example.py
@@ -807,7 +807,8 @@ def get_pokemarkers():
     <span> - </span>
     <b>{name}</b>
 </div>
-<div class='label-countdown' disappears-at='{disappear_time}'>disappears at {disappear_time_formatted}</div>
+<div>disappears at {disappear_time_formatted}</div>
+<div class='label-countdown' disappears-at='{disappear_time}'></div>
 '''
         label = LABEL_TMPL.format(**pokemon)
         #  NOTE: `infobox` field doesn't render multiple line string in frontend
