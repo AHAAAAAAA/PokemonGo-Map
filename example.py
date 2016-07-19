8b5483f9432cb5fee67c2b00843e1530d3fd43db
m4lysh <m4lysh@gmail.com>
2016-07-19 07:32:26 +0200

    update map key

--- example.py
+++ example.py
@@ -35,7 +35,7 @@ API_URL = 'https://pgorelease.nianticlabs.com/plfe/rpc'
 LOGIN_URL = 'https://sso.pokemon.com/sso/login?service=https%3A%2F%2Fsso.pokemon.com%2Fsso%2Foauth2.0%2FcallbackAuthorize'
 LOGIN_OAUTH = 'https://sso.pokemon.com/sso/oauth2.0/accessToken'
 PTC_CLIENT_SECRET = 'w8ScCUXJQc6kXKw8FiOhd8Fixzht18Dq3PEVkUCP5ZPxtgyWsbTvWHFLm2wNY0JR'
-GOOGLEMAPS_KEY = "AIzaSyAZzeHhs-8JZ7i18MjFuM35dJHq70n3Hx4"
+GOOGLEMAPS_KEY = "AIzaSyB66V0uornPsDwpYW9ElWI5I9JTLcd-df0"
 
 SESSION = requests.session()
 SESSION.headers.update({'User-Agent': 'Niantic App'})
