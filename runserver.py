d02b26af24a40f38ac12cd4e0169bc135e17f548
Michael Porter <m_porter@live.co.uk>
2016-07-20 19:53:58 +0100

    Added pokestops and gyms to mock info

--- runserver.py
+++ runserver.py
@@ -10,7 +10,7 @@ from pogom import config
 from pogom.app import Pogom
 from pogom.utils import get_args, insert_mock_data, load_credentials
 from pogom.search import search_loop
-from pogom.models import create_tables, Pokemon
+from pogom.models import create_tables, Pokemon, Pokestop
 from pogom.pgoapi.utilities import get_pos_by_name
 
 log = logging.getLogger(__name__)
@@ -55,7 +55,7 @@ if __name__ == '__main__':
     if not args.mock:
         start_locator_thread(args)
     else:
-        insert_mock_data(args.location, 6)
+        insert_mock_data()
 
     app = Pogom(__name__)
     config['ROOT_PATH'] = app.root_path
