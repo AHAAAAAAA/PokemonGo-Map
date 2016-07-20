0ce62887892c7e4afe50cdbb746948ed495c40bf
Mohammad Edghaim <m.edghaim@gmail.com>
2016-07-20 07:14:12 -0400

    oops. 7am programming fun

--- pogom/app.py
+++ pogom/app.py
@@ -18,7 +18,6 @@ class Pogom(Flask):
         self.route("/pokemons", methods=['GET'])(self.pokemons)
         self.route("/gyms", methods=['GET'])(self.gyms)
         self.route("/pokestops", methods=['GET'])(self.pokestops)
-        self.gmaps_key = None
 
     def fullmap(self):
         return render_template('map.html',
