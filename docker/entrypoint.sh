e78635b96fe53d2318f640db927e6fa74532abc9
Ahmed Osman <ahmed.osman@nordcloud.com>
2016-07-20 09:57:28 +0200

    Support changing values of credentials.json via ENV
    - Use default port instead of forcing 8090
    - Install jq

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,30 @@
-#!/bin/sh
+#!/bin/bash
+
+
+change_api_keys () 
+{
+# Change the API keys in credentials.json 
+# If a variable with the value is provided
+    for VAR in `env`
+    do
+      case "$VAR" in
+          POKEMON_* )
+        key_name=`echo "$VAR" | sed -e "s/^POKEMON_\(.*\)\=.*/\1/"`
+        echo "Changing value of " $key_name
+        key_value=`echo "$VAR" | sed -e "s/.*=\(.*\)/\1/"`
+        cat credentials.json |
+            jq "to_entries |
+                 map(if .key == \"$key_name\"
+                    then . + {\"value\":\"$key_value\"}
+                    else .
+                    end
+                   ) |
+                from_entries" > modified_credentials.json
+        mv modified_credentials.json credentials.json
+        ;;
+        esac
+    done
+}
 
 # If the repo has already been cloned, pull the latest changes
 # Otherwise just clone the repo
@@ -6,13 +32,15 @@ if [ -d "PokemonGo-Map" ]; then
     cd PokemonGo-Map
     git pull
     pip install -r requirements.txt
-    python example.py --port 8090 --host 0.0.0.0 "$@"
+    change_api_keys
+    python example.py --host 0.0.0.0 "$@"
 
 else
     git clone https://github.com/AHAAAAAAA/PokemonGo-Map.git
     cd PokemonGo-Map
     pip install -r requirements.txt
-    python example.py --port 8090 --host 0.0.0.0 "$@"
+    change_api_keys
+    python example.py --host 0.0.0.0 "$@"
 fi
 
 
