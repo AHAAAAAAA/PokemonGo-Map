f6b876c6d9b5114d0f8648db5b830ea2c4a0d62b
Jonas Friedmann <j@frd.mn>
2016-07-20 15:16:43 +0200

    Turn ES6 into plain JS (#536)
    Resolve #526

--- static/map.js
+++ static/map.js
@@ -1,24 +1,27 @@
-
 function pokemonLabel(name, disappear_time, id, disappear_time, latitude, longitude) {
     disappear_date = new Date(disappear_time)
-    let pad = number => number <= 99 ? ("0"+number).slice(-2) : number;
 
-    var label = `
-        <div>
-            <b>${name}</b>
-            <span> - </span>
-            <small>
-                <a href='http://www.pokemon.com/us/pokedex/${id}' target='_blank' title='View in Pokedex'>#${id}</a>
-            </small>
-        </div>
-        <div>
-            Disappears at ${pad(disappear_date.getHours())}:${pad(disappear_date.getMinutes())}:${pad(disappear_date.getSeconds())}
-            <span class='label-countdown' disappears-at='${disappear_time}'>(00m00s)</span></div>
-        <div>
-            <a href='https://www.google.com/maps/dir/Current+Location/${latitude},${longitude}'
-                    target='_blank' title='View in Maps'>Get Directions</a>
-        </div>`;
-    return label;
+    var pad = function pad(number) {
+      return number <= 99 ? ("0" + number).slice(-2) : number;
+    };
+
+    var str = '\
+        <div>\
+            <b>' + name + '</b>\
+            <span> - </span>\
+            <small>\
+                <a href="http://www.pokemon.com/us/pokedex/' + id + '" target="_blank" title="View in Pokedex">#' + id + '</a>\
+            </small>\
+        </div>\
+        <div>\
+            Disappears at ' + pad(disappear_date.getHours()) + ':' + pad(disappear_date.getMinutes()) + ':' + pad(disappear_date.getSeconds()) + '\
+            <span class="label-countdown" disappears-at="' + disappear_time + '">(00m00s)</span></div>\
+        <div>\
+            <a href="https://www.google.com/maps/dir/Current+Location/' + latitude + ',' + longitude + '"\
+                    target="_blank" title="View in Maps">Get Directions</a>\
+        </div>';
+
+    return str;
 }
 
 
