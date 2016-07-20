14cd7a742809d06fb05d7d15966b1cfdbe925f75
Lunat1q <grimfire@gmail.com>
2016-07-20 22:32:41 +0300

    Update map.js

--- static/map.js
+++ static/map.js
@@ -68,11 +68,9 @@ GetNewPokemons = function(stamp) {
                 marker.infoWindow.open(map, marker);
             });
 
-<<<<<<< HEAD
             markers.push({
                     m: marker,
                     disapear: item.disappear_time});
-=======
             marker.addListener('mouseover', function() {
                 marker.infoWindow.open(map, marker);
             });
@@ -82,7 +80,6 @@ GetNewPokemons = function(stamp) {
                     marker.infoWindow.close();
                 }
             });
->>>>>>> refs/remotes/AHAAAAAAA/develop
 
             console.log(item.latitude);
         });        
