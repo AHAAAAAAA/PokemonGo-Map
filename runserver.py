ae0a16d895548a06d45c16c11c3ab31635f22fd9
Chlodochar <Chlodochar@users.noreply.github.com>
2016-07-20 23:36:49 +0200

    use pgoapi from requirements.txt instead of copying its files

--- runserver.py
+++ runserver.py
@@ -15,7 +15,7 @@ from pogom.utils import get_args, insert_mock_data
 from pogom.search import search_loop, create_search_threads
 from pogom.models import init_database, create_tables, Pokemon, Pokestop, Gym
 
-from pogom.pgoapi.utilities import get_pos_by_name
+from pogom.utils import get_pos_by_name
 
 log = logging.getLogger(__name__)
 
