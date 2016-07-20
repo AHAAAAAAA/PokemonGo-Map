dfb1d8294059218896d63b79d091a7c0b6092619
Viktor Suprun <popsul1993@gmail.com>
2016-07-20 20:15:14 +1100

    Fixed 'google is not defined'

--- static/map.js
+++ static/map.js
@@ -2,7 +2,7 @@
 function pokemonLabel(name, disappear_time, id, disappear_time, latitude, longitude) {
     disappear_date = new Date(disappear_time)
     let pad = number => number <= 99 ? ("0"+number).slice(-2) : number;
-    
+
     var label = `
         <div>
             <b>${name}</b>
@@ -12,10 +12,10 @@ function pokemonLabel(name, disappear_time, id, disappear_time, latitude, longit
             </small>
         </div>
         <div>
-            Disappears at ${pad(disappear_date.getHours())}:${pad(disappear_date.getMinutes())}:${pad(disappear_date.getSeconds())} 
+            Disappears at ${pad(disappear_date.getHours())}:${pad(disappear_date.getMinutes())}:${pad(disappear_date.getSeconds())}
             <span class='label-countdown' disappears-at='${disappear_time}'>(00m00s)</span></div>
         <div>
-            <a href='https://www.google.com/maps/dir/Current+Location/${latitude},${longitude}' 
+            <a href='https://www.google.com/maps/dir/Current+Location/${latitude},${longitude}'
                     target='_blank' title='View in Maps'>Get Directions</a>
         </div>`;
     return label;
@@ -25,36 +25,34 @@ function pokemonLabel(name, disappear_time, id, disappear_time, latitude, longit
 var map;
 var marker;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: center_lat, lng: center_lng},
-    zoom: 15
-  });
-  marker = new google.maps.Marker({
-            position: {lat: center_lat, lng: center_lng},
-            map: map,
-            animation: google.maps.Animation.DROP
-        });
-}
-
-$.getJSON("/pokemons", function(result){
-    $.each(result, function(i, item){
-        
-        var marker = new google.maps.Marker({
-            position: {lat: item.latitude, lng: item.longitude},
-            map: map,
-            icon: 'static/icons/'+item.pokemon_id+'.png'
-        });
-        
-        marker.infoWindow = new google.maps.InfoWindow({
-            content: pokemonLabel(item.pokemon_name, item.disappear_time, item.pokemon_id, item.disappear_time, item.latitude, item.longitude)
-        });
-        
-        marker.addListener('click', function() {
-            marker.infoWindow.open(map, marker);
-        });
-
-        console.log(item.latitude);
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: center_lat, lng: center_lng},
+        zoom: 16
     });
-});
+    marker = new google.maps.Marker({
+        position: {lat: center_lat, lng: center_lng},
+        map: map,
+        animation: google.maps.Animation.DROP
+    });
 
+    $.getJSON("/pokemons", function(result){
+        $.each(result, function(i, item){
 
+            var marker = new google.maps.Marker({
+                position: {lat: item.latitude, lng: item.longitude},
+                map: map,
+                icon: 'static/icons/'+item.pokemon_id+'.png'
+            });
+
+            marker.infoWindow = new google.maps.InfoWindow({
+                content: pokemonLabel(item.pokemon_name, item.disappear_time, item.pokemon_id, item.disappear_time, item.latitude, item.longitude)
+            });
+
+            marker.addListener('click', function() {
+                marker.infoWindow.open(map, marker);
+            });
+
+            console.log(item.latitude);
+        });
+    });
+}
