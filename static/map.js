60d061ea9477958b51dd445d07b1422bb9d88e27
Lunat1q <grimfire@gmail.com>
2016-07-20 23:07:45 +0300

    Auto refresh (#636)
    * Added Auto-refresh - Initial
    
    New Pokemons being pushed to the map each 10 seconds.
    Old Pokemons being removed from the map during push.
    
    * Update app.py
    
    * Update map.js
    
    * Update map.html
    
    * Update app.py

--- static/map.js
+++ static/map.js
@@ -1,5 +1,12 @@
-function pokemonLabel(name, disappear_time, id, disappear_time, latitude, longitude) {
-    disappear_date = new Date(disappear_time)
+var map, 
+    marker,
+    lastStamp = 0,
+    requestInterval = 10000;
+
+var markers = [];
+
+pokemonLabel = function(name, disappear_time, id, latitude, longitude) {
+    disappear_date = new Date(disappear_time + (new Date().getTimezoneOffset() * 60000));
 
     var pad = function pad(number) {
       return number <= 99 ? ("0" + number).slice(-2) : number;
@@ -22,23 +29,23 @@ function pokemonLabel(name, disappear_time, id, disappear_time, latitude, longit
         </div>';
 
     return str;
-}
+};
 
-
-var map;
-var marker;
-function initMap() {
+initMap = function() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: center_lat, lng: center_lng},
         zoom: 16
     });
-    marker = new google.maps.Marker({
+    var marker = new google.maps.Marker({
         position: {lat: center_lat, lng: center_lng},
         map: map,
         animation: google.maps.Animation.DROP
-    });
+    });  
+    GetNewPokemons(lastStamp);
+};
 
-    $.getJSON("/pokemons", function(result){
+GetNewPokemons = function(stamp) {
+    $.getJSON("/pokemons/"+stamp, function(result){
         $.each(result, function(i, item){
 
             var marker = new google.maps.Marker({
@@ -48,7 +55,7 @@ function initMap() {
             });
 
             marker.infoWindow = new google.maps.InfoWindow({
-                content: pokemonLabel(item.pokemon_name, item.disappear_time, item.pokemon_id, item.disappear_time, item.latitude, item.longitude)
+                content: pokemonLabel(item.pokemon_name, item.disappear_time, item.pokemon_id, item.latitude, item.longitude)
             });
 
             google.maps.event.addListener(marker.infoWindow, 'closeclick', function(){
@@ -61,6 +68,9 @@ function initMap() {
                 marker.infoWindow.open(map, marker);
             });
 
+            markers.push({
+                    m: marker,
+                    disapear: item.disappear_time});
             marker.addListener('mouseover', function() {
                 marker.infoWindow.open(map, marker);
             });
@@ -72,6 +82,13 @@ function initMap() {
             });
 
             console.log(item.latitude);
-        });
+        });        
+    }).always(function() {setTimeout(function() { GetNewPokemons(lastStamp) }, requestInterval)});
+    var dObj = new Date();
+    lastStamp = dObj.getTime();
+    
+    $.each(markers, function(i, item){
+        if (item.disapear <= lastStamp - (dObj.getTimezoneOffset() * 60000))        
+            item.m.setMap(null);        
     });
-}
+};
