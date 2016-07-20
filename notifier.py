0fba1e845eb0e8d9c14536507d56ad4b1f04ba56
Jack Morris <jxmorris12@users.noreply.github.com>
2016-07-20 14:24:27 -0400

    changed , to & for zoom

--- notifier.py
+++ notifier.py
@@ -35,7 +35,7 @@ def pokemon_found(pokemon):
     print "[+] Notifier found pokemon:", pokename
     address = Nominatim().reverse(str(pokemon["lat"])+", "+str(pokemon["lng"])).address
     # Locate pokemon on GMAPS
-    gMaps = "http://maps.google.com/maps?q=" + str(pokemon["lat"]) + "," + str(pokemon["lng"]) + "," +"24z"
+    gMaps = "http://maps.google.com/maps?q=" + str(pokemon["lat"]) + "," + str(pokemon["lng"]) + "&24z"
     notification_text = "Pokemon Finder found " + _str(pokemon["name"]) + "!"
     disappear_time = str(datetime.fromtimestamp(pokemon["disappear_time"]).strftime("%I:%M%p").lstrip('0'))+")"
     location_text = "Go search at this location: " + address + ". Locate on Google Maps : " + gMaps + ". " + _str(pokemon["name"]) + " will be available until " + disappear_time + "."
