81d071903852570778896f990294b148bb9dd5bc
Everix <nick.a.strub@gmail.com>
2016-07-20 22:27:59 -0500

    Add the ability to specify unwanted pokemon instead of wanted pokemon

--- notifier.py
+++ notifier.py
@@ -9,17 +9,26 @@ sys.setdefaultencoding('utf8')
 
 pushbullet_client = None
 wanted_pokemon = None
+unwanted_pokemon = None
 
 # Initialize object
 def init():
-    global pushbullet_client, wanted_pokemon
+    global pushbullet_client, wanted_pokemon, unwanted_pokemon
     # load pushbullet key
     with open('config.json') as data_file:
         data = json.load(data_file)
         # get list of pokemon to send notifications for
-        wanted_pokemon = _str( data["notify"] ) . split(",")
-        # transform to lowercase
-        wanted_pokemon = [a.lower() for a in wanted_pokemon]
+        if "notify" in data:
+            wanted_pokemon = _str( data["notify"] ) . split(",")
+            
+            # transform to lowercase
+            wanted_pokemon = [a.lower() for a in wanted_pokemon]
+        #get list of pokemon to NOT send notifications for
+        if "do_not_notify" in data:
+            unwanted_pokemon = _str( data["do_not_notify"] ) . split(",")
+            
+            # transform to lowercase
+            unwanted_pokemon = [a.lower() for a in unwanted_pokemon]
         # get api key
         api_key = _str( data["pushbullet"] )
         if api_key:
@@ -35,7 +44,12 @@ def pokemon_found(pokemon):
     # get name
     pokename = _str(pokemon["name"]).lower()
     # check array
-    if not pushbullet_client or not pokename in wanted_pokemon: return
+    if not pushbullet_client:
+        return
+    elif wanted_pokemon != None and not pokename in wanted_pokemon:
+        return
+    elif wanted_pokemon == None and unwanted_pokemon != None and pokename in unwanted_pokemon:
+        return
     # notify
     print "[+] Notifier found pokemon:", pokename
 
