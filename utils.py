50045346c1639bfd7e96ec539115a8acca6ff241
Modrzew <dev@modriv.net>
2016-07-21 09:48:29 +0200

    Accept non-square grids

--- utils.py
+++ utils.py
@@ -5,8 +5,10 @@ def get_start_coords(worker_no):
     """
     Returns center of square for given worker
     """
-    row = worker_no  / config.GRID[0]
-    column = worker_no % config.GRID[1]
+    total_workers = config.GRID[0] * config.GRID[1]
+    per_column = total_workers / config.GRID[0]
+    column = worker_no % per_column
+    row = worker_no / per_column
     part_lat = (config.MAP_END[0] - config.MAP_START[0]) / float(config.GRID[0])
     part_lon = (config.MAP_END[1] - config.MAP_START[1]) / float(config.GRID[1])
     start_lat = config.MAP_START[0] + part_lat * row + part_lat / 2
