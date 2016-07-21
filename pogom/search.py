4939e411731eb17114ab9e20c2927ac613ec13ee
hgentry <harrison.c.gentry@gmail.com>
2016-07-21 22:21:14 +0000

    Stop kicking the server while it's down

--- pogom/search.py
+++ pogom/search.py
@@ -14,6 +14,7 @@ log = logging.getLogger(__name__)
 
 TIMESTAMP = '\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000'
 REQ_SLEEP = 1
+failed_consecutive = 0
 api = PGoApi()
 
 
@@ -81,8 +82,13 @@ def search(args):
 
         try:
             parse_map(response_dict)
+            failed_consecutive = 0
         except KeyError:
             log.error('Scan step failed. Response dictionary key error.')
+            failed_consecutive += 1
+            if(failed_consecutive >= 10):
+            	log.error('Niantic servers under heavy load. Waiting before trying again')
+            	time.sleep(15)
 
         log.info('Completed {:5.2f}% of scan.'.format(float(i) / num_steps**2*100))
         i += 1
