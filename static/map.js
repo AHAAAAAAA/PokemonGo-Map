da26ee1bdc0ad43412eef23498e1f9fe6563af04
Lunat1q <grimfire@gmail.com>
2016-07-21 20:31:35 +0300

    Styling map (#953)
    * Added styles
    * remove useless comments
    
    * css link restore
    
    * css fix

--- static/map.js
+++ static/map.js
@@ -1,124 +1,47 @@
 var map;
 
+var light2Style=[{"elementType":"geometry","stylers":[{"hue":"#ff4400"},{"saturation":-68},{"lightness":-4},{"gamma":0.72}]},{"featureType":"road","elementType":"labels.icon"},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"hue":"#0077ff"},{"gamma":3.1}]},{"featureType":"water","stylers":[{"hue":"#00ccff"},{"gamma":0.44},{"saturation":-33}]},{"featureType":"poi.park","stylers":[{"hue":"#44ff00"},{"saturation":-23}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"hue":"#007fff"},{"gamma":0.77},{"saturation":65},{"lightness":99}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"gamma":0.11},{"weight":5.6},{"saturation":99},{"hue":"#0091ff"},{"lightness":-86}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"lightness":-48},{"hue":"#ff5e00"},{"gamma":1.2},{"saturation":-23}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"saturation":-64},{"hue":"#ff9100"},{"lightness":16},{"gamma":0.47},{"weight":2.7}]}];
+var darkStyle=[{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#b39964"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#181818"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"lightness":17},{"color":"#525252"}]}];
+var pGoStyle=[{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#a1f199"}]},{"featureType":"landscape.natural.landcover","elementType":"geometry.fill","stylers":[{"color":"#37bda2"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry.fill","stylers":[{"color":"#37bda2"}]},{"featureType":"poi.attraction","elementType":"geometry.fill","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","elementType":"geometry.fill","stylers":[{"color":"#e4dfd9"}]},{"featureType":"poi.business","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#37bda2"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#84b09e"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#fafeb8"},{"weight":"1.25"}]},{"featureType":"road.highway","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#5ddad6"}]}];
+
+var selectedStyle = 'light';
+
 function initMap() {
+	
+		
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
             lat: center_lat,
             lng: center_lng
         },
         zoom: 16,
-        styles: [{
-            "featureType": "all",
-            "elementType": "labels.text.fill",
-            "stylers": [{
-                "saturation": 36
-            }, {
-                "color": "#b39964"
-            }, {
-                "lightness": 40
-            }]
-        }, {
-            "featureType": "all",
-            "elementType": "labels.text.stroke",
-            "stylers": [{
-                "visibility": "on"
-            }, {
-                "color": "#000000"
-            }, {
-                "lightness": 16
-            }]
-        }, {
-            "featureType": "all",
-            "elementType": "labels.icon",
-            "stylers": [{
-                "visibility": "off"
-            }]
-        }, {
-            "featureType": "administrative",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 20
-            }]
-        }, {
-            "featureType": "administrative",
-            "elementType": "geometry.stroke",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 17
-            }, {
-                "weight": 1.2
-            }]
-        }, {
-            "featureType": "landscape",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 20
-            }]
-        }, {
-            "featureType": "poi",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 21
-            }]
-        }, {
-            "featureType": "road.highway",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 17
-            }]
-        }, {
-            "featureType": "road.highway",
-            "elementType": "geometry.stroke",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 29
-            }, {
-                "weight": 0.2
-            }]
-        }, {
-            "featureType": "road.arterial",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 18
-            }]
-        }, {
-            "featureType": "road.local",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#181818"
-            }, {
-                "lightness": 16
-            }]
-        }, {
-            "featureType": "transit",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 19
-            }]
-        }, {
-            "featureType": "water",
-            "elementType": "geometry",
-            "stylers": [{
-                "lightness": 17
-            }, {
-                "color": "#525252"
-            }]
-        }]
+        streetViewControl: false,
+		mapTypeControl: true,
+		mapTypeControlOptions: {
+          style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+          mapTypeIds: [
+              google.maps.MapTypeId.ROADMAP,
+              'dark_style',
+              'style_light2',
+              'style_pgo']
+        },
     });
+	
+	var style_dark = new google.maps.StyledMapType(darkStyle, {name: "Dark"});
+	map.mapTypes.set('dark_style', style_dark);
+
+	var style_light2 = new google.maps.StyledMapType(light2Style, {name: "Light2"});
+	map.mapTypes.set('style_light2', style_light2);
+	
+	var style_pgo = new google.maps.StyledMapType(pGoStyle, {name: "PokemonGo"});
+	map.mapTypes.set('style_pgo', style_pgo);
+
+    map.addListener('maptypeid_changed', function(s) {
+        localStorage['map_style'] = this.mapTypeId;
+    }); 
+
+    if (typeof localStorage['map_style'] != undefined)
+	   map.setMapTypeId(localStorage['map_style']);
 
     marker = new google.maps.Marker({
         position: {
@@ -132,7 +55,7 @@ function initMap() {
 };
 
 
-function pokemonLabel(name, disappear_time, id, disappear_time, latitude, longitude) {
+function pokemonLabel(name, id, disappear_time, latitude, longitude) {
     disappear_date = new Date(disappear_time)
     var pad = function (number) { return number <= 99 ? ("0" + number).slice(-2) : number; }
 
@@ -285,54 +208,45 @@ function updateMap() {
         dataType: "json"
     }).done(function(result) {
 
-      $.each(result.pokemons, function(i, item){
-          if (!document.getElementById('pokemon-switch').checked) {
-              return false; // in case the checkbox was unchecked in the meantime.
-          }
-          if (!(item.encounter_id in map_pokemons)) {
-              // add marker to map and item to dict
-              if (item.marker) item.marker.setMap(null);
-              item.marker = setupPokemonMarker(item);
-              map_pokemons[item.encounter_id] = item;
-          }
+        if (document.getElementById('pokemon-switch').checked) {
+            $.each(result.pokemons, function(i, item){                
+                if (!(item.encounter_id in map_pokemons)) {
+                    // add marker to map and item to dict
+                    if (item.marker) item.marker.setMap(null);
+                    item.marker = setupPokemonMarker(item);
+                    map_pokemons[item.encounter_id] = item;
+                }
+            });
+        }
 
-      });
-
-        $.each(result.pokestops, function(i, item) {
-            if (!document.getElementById('pokestops-switch').checked) {
-                return false;
-            } else if (!(item.pokestop_id in map_pokestops)) { // add marker to map and item to dict
-                  // add marker to map and item to dict
-                  if (item.marker) item.marker.setMap(null);
+        if (document.getElementById('pokestops-switch').checked) {
+            $.each(result.pokestops, function (i, item) {
+                if (item.marker) item.marker.setMap(null);
                 item.marker = setupPokestopMarker(item);
                 map_pokestops[item.pokestop_id] = item;
-            }
-
-        });
-
-        $.each(result.gyms, function(i, item){
-            if (!document.getElementById('gyms-switch').checked) {
-                return false; // in case the checkbox was unchecked in the meantime.
-            }
-
-            if (item.gym_id in map_gyms) {
-                // if team has changed, create new marker (new icon)
-                if (map_gyms[item.gym_id].team_id != item.team_id) {
-                    map_gyms[item.gym_id].marker.setMap(null);
-                    map_gyms[item.gym_id].marker = setupGymMarker(item);
-                } else { // if it hasn't changed generate new label only (in case prestige has changed)
-                    map_gyms[item.gym_id].marker.infoWindow = new google.maps.InfoWindow({
-                        content: gymLabel(gym_types[item.team_id], item.team_id, item.gym_points)
-                    });
+            });
+        }
+        
+        if (document.getElementById('gyms-switch').checked) {
+            $.each(result.gyms, function (i, item) {               
+                if (item.gym_id in map_gyms) {
+                    // if team has changed, create new marker (new icon)
+                    if (map_gyms[item.gym_id].team_id != item.team_id) {
+                        map_gyms[item.gym_id].marker.setMap(null);
+                        map_gyms[item.gym_id].marker = setupGymMarker(item);
+                    } else { // if it hasn't changed generate new label only (in case prestige has changed)
+                        map_gyms[item.gym_id].marker.infoWindow = new google.maps.InfoWindow({
+                            content: gymLabel(gym_types[item.team_id], item.team_id, item.gym_points)
+                        });
+                    }
                 }
-            }
-            else { // add marker to map and item to dict
-                if (item.marker) item.marker.setMap(null);
-                item.marker = setupGymMarker(item);
-                map_gyms[item.gym_id] = item;
-            }
-
-        });
+                else { // add marker to map and item to dict
+                    if (item.marker) item.marker.setMap(null);
+                    item.marker = setupGymMarker(item);
+                    map_gyms[item.gym_id] = item;
+                }
+            });
+        }
 
         clearStaleMarkers();
     });
