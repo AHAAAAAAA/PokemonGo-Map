e51a6baa90533f5d6ee46dc9707af1b7b4610436
Matthew Spence <msaspence@gmail.com>
2016-07-22 15:25:47 +0100

    Add an option to runserver.py that enables CORS (#1197)
    This allows clients running on a different host to access the api.

--- pogom/utils.py
+++ pogom/utils.py
@@ -38,6 +38,7 @@ def get_args():
     parser.add_argument('-m', '--mock', help='Mock mode. Starts the web server but not the background thread.', action='store_true', default=False)
     parser.add_argument('-ns', '--no-server', help='No-Server Mode. Starts the searcher but not the Webserver.', action='store_true', default=False, dest='no_server')
     parser.add_argument('-k', '--google-maps-key', help='Google Maps Javascript API Key', default=None, dest='gmaps_key')
+    parser.add_argument('-C', '--cors', help='Enable CORS on web server', action='store_true', default=False)
     parser.set_defaults(DEBUG=False)
     args = parser.parse_args()
     if args.password is None:
