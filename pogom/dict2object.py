c737a15e870708e679b294aeee0e37db1b02099a
Ahmed Almutawa <tolocore@gmail.com>
2016-07-22 04:01:57 -0600

    Revert "Yaml config file" (#1178)

--- pogom/dict2object.py
+++
@@ -1,6 +0,0 @@
-class Dict2Object(dict):
-    def __getattr__(self, name):
-        value = self[name]
-        if isinstance(value, dict):
-            value = Dict2Object(value)
-        return value
