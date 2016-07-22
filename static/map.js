bfdcc531c692beeb5721f92bc2e3eee935a299b3
SpidermanAlliance <joemicara@gmail.com>
2016-07-22 18:23:10 -0400

    Merged

--- static/map.js
+++ static/map.js
@@ -11,10 +11,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
 var $selectExclude = $("#exclude-pokemon");
 var $selectNotify = $("#notify-pokemon");
+var language = document.documentElement.lang == "" ? "en" : document.documentElement.lang;
 
 var idToPokemon = {};
 
-$.getJSON("static/locales/pokemon." + document.documentElement.lang + ".json").done(function(data) {
+$.getJSON("static/locales/pokemon." + language + ".json").done(function(data) {
     var pokeList = []
 
     $.each(data, function(key, value) {
