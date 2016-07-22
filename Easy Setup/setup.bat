7d0edd96e9c3b060a8554bbc94fe23d15c72a714
FrostTheFox <frost@walnutgaming.com>
2016-07-22 11:22:06 -0400

    Rename fix (#1251)
    Readded rename line, to fix for PR #1238

--- Easy Setup/setup.bat
+++ Easy Setup/setup.bat
@@ -38,6 +38,7 @@ popd
 cd ..
 %PATH2%\Scripts\pip install -r requirements.txt
 %PATH2%\Scripts\pip install -r requirements.txt --upgrade
+-rename credentials.example.json credentials.json
 set /p API= Enter your Google API key here:
 
     (
