e0f7c630e457384fe06931305cd9f1424d65f211
igusev <ivgergus@gmail.com>
2016-07-22 10:40:24 -0700

    Click on notification will focus current window instead of opening a new one.
    It will also close the notification.

--- static/map.js
+++ static/map.js
@@ -493,7 +493,8 @@ function sendNotification(title, text, icon) {
         });
 
         notification.onclick = function () {
-            window.open(window.location.href);
+            window.focus();
+            notification.close();
         };
     }
 }
