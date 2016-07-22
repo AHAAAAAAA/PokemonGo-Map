2664665e97c751d298d85c1a8727c5b7b90c8122
ravishivt <javishi@arubanetworks.com>
2016-07-21 23:07:26 -0700

    Cleaning up JS files and reorganizing build process. part 2

--- static/js/app.js
+++ static/js/app.js
@@ -5,13 +5,15 @@
 	// Methods/polyfills.
 
 	// addEventsListener
-	var addEventsListener=function(o,t,e){var n,i=t.split(" ");for(n in i)o.addEventListener(i[n],e)}
+	let addEventsListener=function(o,t,e){let n,i=t.split(" ");for(n in i)o.addEventListener(i[n],e);};
 
+        /* jshint ignore:start */
 	// classList | (c) @remy | github.com/remy/polyfills | rem.mit-license.org
-	!function(){function t(t){this.el=t;for(var n=t.className.replace(/^\s+|\s+$/g,"").split(/\s+/),i=0;i<n.length;i++)e.call(this,n[i])}function n(t,n,i){Object.defineProperty?Object.defineProperty(t,n,{get:i}):t.__defineGetter__(n,i)}if(!("undefined"==typeof window.Element||"classList"in document.documentElement)){var i=Array.prototype,e=i.push,s=i.splice,o=i.join;t.prototype={add:function(t){this.contains(t)||(e.call(this,t),this.el.className=this.toString())},contains:function(t){return-1!=this.el.className.indexOf(t)},item:function(t){return this[t]||null},remove:function(t){if(this.contains(t)){for(var n=0;n<this.length&&this[n]!=t;n++);s.call(this,n,1),this.el.className=this.toString()}},toString:function(){return o.call(this," ")},toggle:function(t){return this.contains(t)?this.remove(t):this.add(t),this.contains(t)}},window.DOMTokenList=t,n(Element.prototype,"classList",function(){return new t(this)})}}();
+	!function(){function t(t){this.el=t;for(let n=t.className.replace(/^\s+|\s+$/g,"").split(/\s+/),i=0;i<n.length;i++)e.call(this,n[i])}function n(t,n,i){Object.defineProperty?Object.defineProperty(t,n,{get:i}):t.__defineGetter__(n,i)}if(!("undefined"==typeof window.Element||"classList"in document.documentElement)){let i=Array.prototype,e=i.push,s=i.splice,o=i.join;t.prototype={add:function(t){this.contains(t)||(e.call(this,t),this.el.className=this.toString())},contains:function(t){return-1!=this.el.className.indexOf(t)},item:function(t){return this[t]||null},remove:function(t){if(this.contains(t)){for(let n=0;n<this.length&&this[n]!=t;n++);s.call(this,n,1),this.el.className=this.toString()}},toString:function(){return o.call(this," ")},toggle:function(t){return this.contains(t)?this.remove(t):this.add(t),this.contains(t)}},window.DOMTokenList=t,n(Element.prototype,"classList",function(){return new t(this)})}}();
+        /* jshint ignore:end */
 
 	// Vars.
-	var	$body = document.querySelector('body');
+	let $body = document.querySelector('body');
 
 	// Breakpoints.
 	skel.breakpoints({
@@ -30,11 +32,11 @@
 	});
 
 	// Nav.
-	var	$nav = document.querySelector('#nav'),
-		$navToggle = document.querySelector('a[href="#nav"]'),
-		$navClose;
+	var $nav = document.querySelector('#nav'),
+	    $navToggle = document.querySelector('a[href="#nav"]'),
+	    $navClose;
 
-	var $overlay = document.querySelector('#trigger-overlay');
+	let $overlay = document.querySelector('#trigger-overlay');
 
 	// Event: Prevent clicks/taps inside the nav from bubbling.
 	addEventsListener($nav, 'click touchend', function(event) {
