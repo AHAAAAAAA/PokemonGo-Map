16198f01377671a0a38cb24ca9d28c6e8b6f3853
Nikita Golubov <neekitkah@gmail.com>
2016-07-23 14:09:43 +0300

    add exclude and notify links to pokemon info popup

--- static/map.js
+++ static/map.js
@@ -55,6 +55,18 @@ $selectNotify.on("change", function (e) {
     localStorage.remember_select_notify = JSON.stringify(notifiedPokemon);
 });
 
+function excludePokemon(id) {
+    $selectExclude.val(
+        $selectExclude.val().concat(id)
+    ).trigger('change')
+}
+
+function notifyAboutPokemon(id) {
+    $selectNotify.val(
+        $selectNotify.val().concat(id)
+    ).trigger('change')
+}
+
 var map;
 
 var light2Style=[{"elementType":"geometry","stylers":[{"hue":"#ff4400"},{"saturation":-68},{"lightness":-4},{"gamma":0.72}]},{"featureType":"road","elementType":"labels.icon"},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"hue":"#0077ff"},{"gamma":3.1}]},{"featureType":"water","stylers":[{"hue":"#00ccff"},{"gamma":0.44},{"saturation":-33}]},{"featureType":"poi.park","stylers":[{"hue":"#44ff00"},{"saturation":-23}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"hue":"#007fff"},{"gamma":0.77},{"saturation":65},{"lightness":99}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"gamma":0.11},{"weight":5.6},{"saturation":99},{"hue":"#0091ff"},{"lightness":-86}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"lightness":-48},{"hue":"#ff5e00"},{"gamma":1.2},{"saturation":-23}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"saturation":-64},{"hue":"#ff9100"},{"lightness":16},{"gamma":0.47},{"weight":2.7}]}];
@@ -161,6 +173,8 @@ function pokemonLabel(name, disappear_time, id, latitude, longitude) {
             Disappears at ${pad(disappear_date.getHours())}:${pad(disappear_date.getMinutes())}:${pad(disappear_date.getSeconds())}
             <span class='label-countdown' disappears-at='${disappear_time}'>(00m00s)</span></div>
         <div>
+            <a href='javascript:excludePokemon(${id})'>Exclude</a>&nbsp;&nbsp;
+            <a href='javascript:notifyAboutPokemon(${id})'>Notify</a>&nbsp;&nbsp;
             <a href='https://www.google.com/maps/dir/Current+Location/${latitude},${longitude}'
                     target='_blank' title='View in Maps'>Get directions</a>
         </div>`;
