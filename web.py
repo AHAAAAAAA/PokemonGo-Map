9c0bd50ab0724b98518188bbf5a9bef12f210a8f
Modrzew <dev@modriv.net>
2016-07-23 18:53:49 +0200

    Removed debug code

--- web.py
+++ web.py
@@ -91,9 +91,7 @@ def get_pokemarkers():
     markers = []
 
     workers = app_config.GRID[0] * app_config.GRID[1]
-    for i, icon in zip(range(workers), icons.alpha.options):
-        if icon == 'KL':
-            icon = 'W'
+    for i in range(workers):
         coords = utils.get_start_coords(i)
         markers.append({
             'icon': icons.dots.red,
@@ -104,16 +102,6 @@ def get_pokemarkers():
             'key': 'start-position-%d' % i,
             'disappear_time': -1
         })
-        # for no, point in enumerate(utils.get_worker_grid(i)):
-        #     markers.append({
-        #         'icon': getattr(icons.alpha, icon),
-        #         'lat': point[0],
-        #         'lng': point[1],
-        #         'infobox': "Worker %d point %d" % (i, no),
-        #         'type': 'custom',
-        #         'key': 'worker-%d-point-%d' % (i, no),
-        #         'disappear_time': -1
-        #     })
     return markers
 
     session = db.Session()
