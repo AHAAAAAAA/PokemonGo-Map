6e331561c04862246d0f80396463d8f1af4610bd
Samuel Bétrisey <me@sam.ninja>
2016-07-23 14:23:48 +0200

    Fix Google Maps direction URL for foreign languages
    "Current Location" only works in english and there isn't any other universal keyword so I fixed it by forcing the language
    
    Fixes #1503

--- static/map.js
+++ static/map.js
@@ -161,7 +161,7 @@ function pokemonLabel(name, disappear_time, id, latitude, longitude) {
             Disappears at ${pad(disappear_date.getHours())}:${pad(disappear_date.getMinutes())}:${pad(disappear_date.getSeconds())}
             <span class='label-countdown' disappears-at='${disappear_time}'>(00m00s)</span></div>
         <div>
-            <a href='https://www.google.com/maps/dir/Current+Location/${latitude},${longitude}'
+            <a href='https://www.google.com/maps/dir/Current+Location/${latitude},${longitude}?hl=en'
                     target='_blank' title='View in Maps'>Get directions</a>
         </div>`;
     return contentstring;
@@ -221,7 +221,7 @@ function pokestopLabel(lured, last_modified, active_pokemon_id, latitude, longit
                 <span class='label-countdown' disappears-at='${expire_time}'>(00m00s)</span></div>
             <div>
             <div>
-                <a href='https://www.google.com/maps/dir/Current+Location/${latitude},${longitude}'
+                <a href='https://www.google.com/maps/dir/Current+Location/${latitude},${longitude}?hl=en'
                         target='_blank' title='View in Maps'>Get directions</a>
             </div>`;
     } else {
@@ -230,7 +230,7 @@ function pokestopLabel(lured, last_modified, active_pokemon_id, latitude, longit
                 <b>Pokéstop</b>
             </div>
             <div>
-                <a href='https://www.google.com/maps/dir/Current+Location/${latitude},${longitude}'
+                <a href='https://www.google.com/maps/dir/Current+Location/${latitude},${longitude}?hl=en'
                         target='_blank' title='View in Maps'>Get directions</a>
             </div>`;
     }
