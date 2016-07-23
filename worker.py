37e9d953fee6028a14eede946313509b9e39cb49
Modrzew <dev@modriv.net>
2016-07-23 13:59:56 +0200

    Fancy status bar, improved logging and more clean up
    Status bar shows status of all workers at the same time and is turned on
    by default. Logs are put into worker.log instead.
    
    If you don't want status bar, use --no-status-bar command line option.
    Logs will be put in the console and not in the file.
    
    Oh, and worker.py is finally pep8-compatible! \o/

--- worker.py
+++ worker.py
@@ -3,11 +3,12 @@
 from datetime import datetime
 import argparse
 import json
+import logging
 import os
 import random
 import re
-import sys
 import struct
+import sys
 import threading
 import time
 
@@ -30,7 +31,10 @@ import utils
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 API_URL = 'https://pgorelease.nianticlabs.com/plfe/rpc'
-LOGIN_URL = 'https://sso.pokemon.com/sso/login?service=https://sso.pokemon.com/sso/oauth2.0/callbackAuthorize'
+LOGIN_URL = (
+    'https://sso.pokemon.com/sso/login?service='
+    'https://sso.pokemon.com/sso/oauth2.0/callbackAuthorize'
+)
 LOGIN_OAUTH = 'https://sso.pokemon.com/sso/oauth2.0/accessToken'
 APP = 'com.nianticlabs.pokemongo'
 
@@ -65,10 +69,22 @@ class CannotGetProfile(Exception):
     """
 
 
+def configure_logger(filename='worker.log'):
+    logging.basicConfig(
+        filename=filename,
+        format=(
+            '[%(asctime)s][%(threadName)10s][%(levelname)8s][L%(lineno)4d] '
+            '%(message)s'
+        ),
+        style='{',
+        level=logging.INFO,
+    )
+
+logger = logging.getLogger()
+
+
 def debug(message):
-    return
-    if DEBUG:
-        print '[-] {}'.format(message)
+    logger.info(message)
 
 
 def encode(cellid):
@@ -124,14 +140,21 @@ def get_location_coords():
 def retrying_api_req(service, api_endpoint, access_token, *args, **kwargs):
     while True:
         try:
-            response = api_req(service, api_endpoint, access_token, *args,
-                               **kwargs)
+            response = api_req(
+                service,
+                api_endpoint,
+                access_token,
+                *args,
+                **kwargs
+            )
             if response:
                 return response
-            debug('retrying_api_req: api_req returned None, retrying')
-        except (InvalidURL, ConnectionError, DecodeError), e:
-            debug('retrying_api_req: request error ({}), retrying'.format(
-                str(e)))
+            logger.debug('retrying_api_req: api_req returned None, retrying')
+        except (InvalidURL, ConnectionError, DecodeError) as e:
+            logger.debug(
+                'retrying_api_req: request error (%s), retrying',
+                str(e)
+            )
         time.sleep(1)
 
 
@@ -179,15 +202,19 @@ def api_req(service, api_endpoint, access_token, *args, **kwargs):
 def get_api_endpoint(service, access_token, api=API_URL):
     profile_response = None
     while not profile_response:
-        profile_response = retrying_get_profile(service, access_token, api, None)
+        profile_response = retrying_get_profile(
+            service,
+            access_token,
+            api,
+            None
+        )
         if not hasattr(profile_response, 'api_url'):
-            debug('retrying_get_profile: get_profile returned no api_url, retrying')
+            logger.debug('get_profile returned no api_url, retrying')
             profile_response = None
             continue
         if not len(profile_response.api_url):
-            debug('get_api_endpoint: retrying_get_profile returned no-len api_url, retrying')
+            logger.debug('get_profile returned no-len api_url, retrying')
             profile_response = None
-
     return 'https://%s/rpc' % profile_response.api_url
 
 
@@ -201,7 +228,7 @@ def retrying_get_profile(service, access_token, api, useauth, *reqq):
             service, access_token, api, useauth, *reqq
         )
         if not getattr(profile_response, 'payload'):
-            debug('retrying_get_profile: get_profile returned no or no-len payload, retrying')
+            logger.debug('get_profile returned no or no-len payload, retrying')
             profile_response = None
             times_tried += 1
             continue
@@ -238,33 +265,33 @@ def get_profile(service, access_token, api, useauth, *reqq):
 
 
 def login_google(username, password):
-    debug('[!] Google login for: {}'.format(username))
+    logger.info('Google login for: %s', username)
     r1 = perform_master_login(username, password, ANDROID_ID)
-    r2 = perform_oauth(username,
-                       r1.get('Token', ''),
-                       ANDROID_ID,
-                       SERVICE,
-                       APP,
-                       CLIENT_SIG, )
+    r2 = perform_oauth(
+        username,
+        r1.get('Token', ''),
+        ANDROID_ID,
+        SERVICE,
+        APP,
+        CLIENT_SIG,
+    )
     return r2.get('Auth')
 
 
 def login_ptc(username, password):
-    debug('[!] PTC login for: {}'.format(username))
+    logger.info('PTC login for: %s', username)
     head = {'User-Agent': 'Niantic App'}
     session = local_data.api_session
     r = session.get(LOGIN_URL, headers=head)
-
     try:
         jdata = json.loads(r.content)
     except ValueError:
-        debug('login_ptc: could not decode JSON from {}'.format(r.content))
+        logger.warning('login_ptc: could not decode JSON from %s', r.content)
         return None
-
-    # Maximum password length is 15 (sign in page enforces this limit, API does not)
-
+    # Maximum password length is 15
+    # (sign in page enforces this limit, API does not)
     if len(password) > 15:
-        debug('[!] Trimming password to 15 characters')
+        logger.debug('Trimming password to 15 characters')
         password = password[:15]
 
     data = {
@@ -280,8 +307,7 @@ def login_ptc(username, password):
     try:
         ticket = re.sub('.*ticket=', '', r1.history[0].headers['Location'])
     except Exception:
-        if DEBUG:
-            debug(r1.json()['errors'][0])
+        logger.debug('Error: %s', r1.json()['errors'][0])
         return None
 
     data1 = {
@@ -294,14 +320,10 @@ def login_ptc(username, password):
     r2 = session.post(LOGIN_OAUTH, data=data1)
     access_token = re.sub('&expires.*', '', r2.content)
     access_token = re.sub('.*access_token=', '', access_token)
-
     return access_token
 
 
-def get_heartbeat(service,
-                  api_endpoint,
-                  access_token,
-                  response, ):
+def get_heartbeat(service, api_endpoint, access_token, response):
     m4 = pokemon_pb2.RequestEnvelop.Requests()
     m = pokemon_pb2.RequestEnvelop.MessageSingleInt()
     m.f1 = int(time.time() * 1000)
@@ -315,20 +337,24 @@ def get_heartbeat(service,
     m1.type = 106
     m = pokemon_pb2.RequestEnvelop.MessageQuad()
     m.f1 = ''.join(map(encode, walk))
-    m.f2 = \
-        "\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000"
+    m.f2 = (
+        '\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000'
+        '\000\000\000\000'
+    )
     m.lat = COORDS_LATITUDE
     m.long = COORDS_LONGITUDE
     m1.message = m.SerializeToString()
-    response = get_profile(service,
-                           access_token,
-                           api_endpoint,
-                           response.unknown7,
-                           m1,
-                           pokemon_pb2.RequestEnvelop.Requests(),
-                           m4,
-                           pokemon_pb2.RequestEnvelop.Requests(),
-                           m5, )
+    response = get_profile(
+        service,
+        access_token,
+        api_endpoint,
+        response.unknown7,
+        m1,
+        pokemon_pb2.RequestEnvelop.Requests(),
+        m4,
+        pokemon_pb2.RequestEnvelop.Requests(),
+        m5,
+    )
 
     try:
         payload = response.payload[0]
@@ -341,30 +367,16 @@ def get_heartbeat(service,
 
 
 def get_token(service, username, password):
-    """
-    Get token if it's not None
-    :return:
-    :rtype:
-    """
     if service == 'ptc':
         global_token = None
         while not global_token:
             global_token = login_ptc(username, password)
             if not global_token:
-                debug('Could not login to PTC - sleeping')
+                logger.info('Could not login to PTC - sleeping')
                 time.sleep(random.randint(10, 20))
     else:
         global_token = login_google(username, password)
     return global_token
-
-
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-st', '--step-limit', help='Steps', required=True)
-    parser.add_argument(
-        '-d', '--debug', help='Debug Mode', action='store_true')
-    parser.set_defaults(DEBUG=True)
-    return parser.parse_args()
 
 
 def login(username, password, service):
@@ -372,33 +384,40 @@ def login(username, password, service):
     if access_token is None:
         raise Exception('[-] Wrong username/password')
 
-    debug('[+] RPC Session Token: {} ...'.format(access_token[:25]))
+    logger.debug('RPC Session Token: %s...', access_token[:25])
 
     api_endpoint = get_api_endpoint(service, access_token)
     if api_endpoint is None:
         raise Exception('[-] RPC server offline')
 
-    debug('[+] Received API endpoint: {}'.format(api_endpoint))
+    logger.debug('Received API endpoint: %s', api_endpoint)
 
-    profile_response = retrying_get_profile(service, access_token,
-                                            api_endpoint, None)
+    profile_response = retrying_get_profile(
+        service,
+        access_token,
+        api_endpoint,
+        None,
+    )
     if profile_response is None or not profile_response.payload:
         raise Exception('Could not get profile')
 
-    debug('[+] Login successful')
+    logger.info('Login successful')
 
     payload = profile_response.payload[0]
     profile = pokemon_pb2.ResponseEnvelop.ProfilePayload()
     profile.ParseFromString(payload)
-    debug('[+] Username: {}'.format(profile.profile.username))
+    logger.debug('Username: %s', profile.profile.username)
 
-    creation_time = \
-        datetime.fromtimestamp(int(profile.profile.creation_time) / 1000)
-    debug('[+] You started playing Pokemon Go on: {}'.format(
-        creation_time.strftime('%Y-%m-%d %H:%M:%S')))
+    creation_time = datetime.fromtimestamp(
+        int(profile.profile.creation_time) / 1000
+    )
+    logger.debug(
+        'You started playing Pokemon Go on: %s',
+        creation_time.strftime('%Y-%m-%d %H:%M:%S')
+    )
 
     for curr in profile.profile.currency:
-        debug('[+] {}: {}'.format(curr.type, curr.amount))
+        logger.debug('%s: %s', curr.type, curr.amount)
 
     return api_endpoint, access_token, profile_response
 
@@ -407,6 +426,7 @@ class Slave(threading.Thread):
     def __init__(self, group=None, target=None, name=None, worker_no=None):
         super(Slave, self).__init__(group, target, name)
         self.worker_no = worker_no
+        local_data.worker_no = worker_no
         self.step = 0
         self.cycle = 0
 
@@ -446,7 +466,7 @@ class Slave(threading.Thread):
     def main(self, service, api_endpoint, access_token, profile_response):
         origin_lat, origin_lon = utils.get_start_coords(self.worker_no)
 
-        args = get_args()
+        args = parse_args()
 
         steplimit = int(args.step_limit)
 
@@ -462,7 +482,10 @@ class Slave(threading.Thread):
             add_to_db = []
             self.step = step + 1
             # Scan location math
-            if -steplimit2 / 2 < x <= steplimit2 / 2 and -steplimit2 / 2 < y <= steplimit2 / 2:
+            if (
+                -steplimit2 / 2 < x <= steplimit2 / 2 and
+                -steplimit2 / 2 < y <= steplimit2 / 2
+            ):
                 lat = x * 0.0025 + origin_lat
                 lon = y * 0.0025 + origin_lon
             if x == y or x < 0 and x == -y or x > 0 and x == 1 - y:
@@ -486,7 +509,10 @@ class Slave(threading.Thread):
                 seen += 1
             session.commit()
             add_to_db = []
-            debug('Completed: ' + str(((step+1) + pos * .25 - .25) / (steplimit2) * 100) + '%')
+            logger.info(
+                'Completed: %s%%',
+                ((step + 1) + pos * .25 - .25) / (steplimit2) * 100
+            )
             # Clear error code and let know that there are Pokemon
             if self.error_code and seen:
                 self.error_code = None
@@ -510,7 +536,7 @@ class Slave(threading.Thread):
 def process_step(
     service, api_endpoint, access_token, profile_response, add_to_db, lat, lon
 ):
-    debug('[+] Searching for Pokemon at location {} {}'.format(lat, lon))
+    logger.debug('Searching for Pokemon at location %s %s', lat, lon)
     step_lat = lat
     step_long = lon
     parent = CellId.from_lat_lng(
@@ -528,9 +554,12 @@ def process_step(
     for child in parent.children():
         latlng = LatLng.from_point(Cell(child).get_center())
         set_location_coords(latlng.lat().degrees, latlng.lng().degrees, 0)
-        hs.append(
-            get_heartbeat(service, api_endpoint, access_token,
-                          profile_response))
+        hs.append(get_heartbeat(
+            service,
+            api_endpoint,
+            access_token,
+            profile_response
+        ))
     set_location_coords(step_lat, step_long, 0)
     visible = []
 
@@ -549,17 +578,21 @@ def process_step(
     for poke in visible:
         disappear_timestamp = time.time() + poke.TimeTillHiddenMs / 1000
         pokemons[poke.SpawnPointId] = {
-            "lat": poke.Latitude,
-            "lng": poke.Longitude,
-            "disappear_time": disappear_timestamp,
-            "id": poke.pokemon.PokemonId,
+            'lat': poke.Latitude,
+            'lng': poke.Longitude,
+            'disappear_time': disappear_timestamp,
+            'id': poke.pokemon.PokemonId,
         }
         add_to_db.append(poke.SpawnPointId)
 
 
-def get_status_message(workers, count):
+def get_status_message(workers, count, start_time):
     messages = [workers[i].status.ljust(20) for i in range(count)]
-    output = []
+    running_for = datetime.now() - start_time
+    output = [
+        'PokeMiner\trunning for {}'.format(running_for),
+        ''
+    ]
     previous = 0
     for i in range(4, count + 1, 4):
         output.append('\t'.join(messages[previous:i]))
@@ -570,22 +603,60 @@ def get_status_message(workers, count):
 def start_worker(worker_no):
     # Ok I NEED to global this here
     global workers
-    debug('[W%d] Worker (re)starting up!' % worker_no)
+    logger.info('Worker (re)starting up!')
     worker = Slave(name='worker-%d' % worker_no, worker_no=worker_no)
     worker.daemon = True
     worker.start()
     workers[worker_no] = worker
 
 
-def spawn_workers(workers):
+def spawn_workers(workers, status_bar=True):
+    start_time = datetime.now()
     count = config.GRID[0] * config.GRID[1]
     for worker_no in range(count):
         start_worker(worker_no)
     while True:
-        _ = os.system('clear')
-        print get_status_message(workers, count)
+        if status_bar:
+            if sys.platform == 'win32':
+                _ = os.system('cls')
+            else:
+                _ = os.system('clear')
+            print get_status_message(workers, count, start_time)
         time.sleep(0.5)
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-st',
+        '--step-limit',
+        help=(
+            'Steps limit - area around worker in which it will look '
+            'for Pokemon'
+        ),
+        required=True
+    )
+    parser.add_argument(
+        '--no-status-bar',
+        dest='status_bar',
+        help='Log to console instead of displaying status bar',
+        action='store_false',
+    )
+    parser.add_argument(
+        '--log-level',
+        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR'],
+        default=logging.INFO
+    )
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    spawn_workers(workers)
+    args = parse_args()
+    logger.setLevel(args.log_level)
+    if args.status_bar:
+        configure_logger(filename='worker.log')
+        logger.info('-' * 30)
+        logger.info('Starting up!')
+    else:
+        configure_logger(filename=None)
+    spawn_workers(workers, status_bar=args.status_bar)
