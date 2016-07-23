056cf6d3fae2bf348f2af23e3586264621ed4380
kmiller <ken@miller.ec>
2016-07-23 02:56:00 +0200

    Added marker and 'My location' button for showing and centering on users' geo location

--- static/map.js
+++ static/map.js
@@ -61,6 +61,60 @@ var pGoStyle=[{"featureType":"landscape.man_made","elementType":"geometry.fill",
 
 var selectedStyle = 'light';
 
+function addYourLocationButton (map, loc_marker)
+{
+    var controlDiv = document.createElement('div');
+
+    var firstChild = document.createElement('button');
+    firstChild.style.backgroundColor = '#fff';
+    firstChild.style.border = 'none';
+    firstChild.style.outline = 'none';
+    firstChild.style.width = '28px';
+    firstChild.style.height = '28px';
+    firstChild.style.borderRadius = '2px';
+    firstChild.style.boxShadow = '0 1px 4px rgba(0,0,0,0.3)';
+    firstChild.style.cursor = 'pointer';
+    firstChild.style.marginRight = '10px';
+    firstChild.style.padding = '0';
+    firstChild.title = 'Your Location';
+    controlDiv.appendChild(firstChild);
+
+    var secondChild = document.createElement('div');
+    secondChild.style.margin = '5px';
+    secondChild.style.width = '18px';
+    secondChild.style.height = '18px';
+    secondChild.style.backgroundImage = 'url(https://maps.gstatic.com/tactile/mylocation/mylocation-sprite-2x.png)';
+    secondChild.style.backgroundSize = '180px 18px';
+    secondChild.style.backgroundPosition = '0 0';
+    secondChild.style.backgroundRepeat = 'no-repeat';
+    firstChild.appendChild(secondChild);
+
+    google.maps.event.addListener(map, 'center_changed', function () {
+        secondChild.style['background-position'] = '0 0';
+    });
+
+    firstChild.addEventListener('click', function () {
+        var imgX = '0',
+            animationInterval = setInterval(function () {
+                imgX = imgX === '-18' ? '0' : '-18';
+                secondChild.style['background-position'] = imgX+'px 0';
+            }, 500);
+
+        if(loc_marker) {
+            var latlng = loc_marker.getPosition();
+            map.setCenter(latlng);
+            clearInterval(animationInterval);
+            secondChild.style['background-position'] = '-144px 0';
+        } else {
+            clearInterval(animationInterval);
+            secondChild.style['background-position'] = '0 0';
+        }
+    });
+
+    controlDiv.index = 1;
+    map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(controlDiv);
+}
+
 function initMap() {
 
 
@@ -84,6 +138,12 @@ function initMap() {
               'style_pgo']
         },
     });
+
+        $.getScript('https://github.com/ChadKillingsworth/geolocation-marker/releases/download/v2.0.4/geolocation-marker.js', function()
+        {
+            var loc_marker = new GeolocationMarker(map);
+            addYourLocationButton(map, loc_marker);
+        });
 
 	var style_dark = new google.maps.StyledMapType(darkStyle, {name: "Dark"});
 	map.mapTypes.set('dark_style', style_dark);
