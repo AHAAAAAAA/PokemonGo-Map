a5cba1aa233d6f9fd99ed7443a99ced67ea65e97
Jeff Minard <chuyskywalker@users.noreply.github.com>
2016-07-22 18:59:12 -0700

    Use localstorage for exclude/notify pokemon lists (#1390)
    In #1192 a user is having trouble keeping these lists, but the localstorage options do work. Since theres no real reason to have these as cookies, I've converted the lists over to localstorage instead.

--- static/map.js
+++ static/map.js
@@ -22,19 +22,23 @@ $.getJSON("static/locales/pokemon." + document.documentElement.lang + ".json").d
         idToPokemon[key] = value;
     });
 
-    JSON.parse(readCookie("remember_select_exclude"));
+    // setup the filter lists
     $selectExclude.select2({
         placeholder: "Select Pokémon",
         data: pokeList
     });
-    $selectExclude.val(JSON.parse(readCookie("remember_select_exclude"))).trigger("change");
-
-    JSON.parse(readCookie("remember_select_notify"));
     $selectNotify.select2({
         placeholder: "Select Pokémon",
         data: pokeList
     });
-    $selectNotify.val(JSON.parse(readCookie("remember_select_notify"))).trigger("change");
+
+    // recall saved lists
+    if (localStorage['remember_select_exclude']) {
+        $selectExclude.val(JSON.parse(localStorage.remember_select_exclude)).trigger("change");
+    }
+    if (localStorage['remember_select_notify']) {
+        $selectNotify.val(JSON.parse(localStorage.remember_select_notify)).trigger("change");
+    }
 });
 
 var excludedPokemon = [];
@@ -43,14 +47,12 @@ var notifiedPokemon = [];
 $selectExclude.on("change", function (e) {
     excludedPokemon = $selectExclude.val().map(Number);
     clearStaleMarkers();
-    document.cookie = 'remember_select_exclude='+JSON.stringify(excludedPokemon)+
-            '; max-age=31536000; path=/';
+    localStorage.remember_select_exclude = JSON.stringify(excludedPokemon);
 });
 
 $selectNotify.on("change", function (e) {
     notifiedPokemon = $selectNotify.val().map(Number);
-    document.cookie = 'remember_select_notify='+JSON.stringify(notifiedPokemon)+
-            '; max-age=31536000; path=/';
+    localStorage.remember_select_notify = JSON.stringify(notifiedPokemon);
 });
 
 var map;
@@ -554,17 +556,6 @@ var updateLabelDiffTime = function() {
 };
 
 window.setInterval(updateLabelDiffTime, 1000);
-
-function readCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
-}
 
 function sendNotification(title, text, icon) {
     if (Notification.permission !== "granted") {
