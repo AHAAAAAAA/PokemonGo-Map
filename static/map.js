6fe3e757a83d0d9ab352b2bb8c2276316c6711d2
unknown <retalyx@retalyx.fr>
2016-07-23 07:28:39 +0200

    Pokémons are now ordered by names

--- static/map.js
+++ static/map.js
@@ -390,30 +390,48 @@ function clearStaleMarkers() {
     });
 };
 
+var sort_by = function(field, reverse, primer){
+
+   var key = primer ? 
+       function(x) {return primer(x[field])} : 
+       function(x) {return x[field]};
+
+   reverse = !reverse ? 1 : -1;
+
+   return function (a, b) {
+       return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
+     } 
+}
+
 function countMarkers() {
 	arenaCount = [];
 	arenaTotal = 0;
 	pkmnCount = [];
-	pkmnName = [];
 	pkmnTotal = 0;
 	pokestopCount = [];
 	pokestopTotal = 0;
     $.each(map_pokemons, function(key, value) {
 		if (pkmnCount[map_pokemons[key]['pokemon_id']] == 0 || pkmnCount[map_pokemons[key]['pokemon_id']] == null )
 		{
-			pkmnCount[map_pokemons[key]['pokemon_id']] = 1;
-			pkmnName[map_pokemons[key]['pokemon_id']] = map_pokemons[key]['pokemon_name'];
+			pkmnCount[map_pokemons[key]['pokemon_id']] = {
+				"ID": map_pokemons[key]['pokemon_id'],
+				"Count": 1,
+				"Name": map_pokemons[key]['pokemon_name']
+			};
 		}
 		else
 		{
-			pkmnCount[map_pokemons[key]['pokemon_id']] += 1;
+			pkmnCount[map_pokemons[key]['pokemon_id']].Count += 1;
 		}
 		pkmnTotal++;
+    });
+    pkmnCount.sort(sort_by('Name', false));
+    $.each(map_pokemons, function(key, value) {
 		var pkmnListString = "<table><thead><tr><th>Icon</th><th>Name</th><th>Count</th><th>%</th></tr></thead><tbody><tr><td></td><td>Total</td><td>"+pkmnTotal+"</td></tr>";
 		for(var i=0;i < pkmnCount.length;i++) {
-			if (pkmnCount[i] > 0)
+			if (pkmnCount[i] != null && pkmnCount[i].Count > 0)
 			{
-				pkmnListString += "<tr><td><img src=\"/static/icons/" + i + ".png\" /></td><td><a href='http://www.pokemon.com/us/pokedex/" + i + "' target='_blank' title='View in Pokedex' style=\"color: black;\">" + pkmnName[i] + "</a></td><td>" + pkmnCount[i] + "</td><td>"+Math.round(pkmnCount[i]*100/pkmnTotal*10)/10+"%</td></tr>";
+				pkmnListString += "<tr><td><img src=\"/static/icons/" + pkmnCount[i].ID + ".png\" /></td><td><a href='http://www.pokemon.com/us/pokedex/" + pkmnCount[i].ID + "' target='_blank' title='View in Pokedex' style=\"color: black;\">" + pkmnCount[i].Name + "</a></td><td>" + pkmnCount[i].Count + "</td><td>"+Math.round(pkmnCount[i].Count*100/pkmnTotal*10)/10+"%</td></tr>";
 			}
 		}
 		pkmnListString += "</tbody></table>";
