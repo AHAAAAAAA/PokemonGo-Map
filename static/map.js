0dceeeff538e485f358ea3099363dabd9b2b1a91
Alex Yang <yg398701842@live.cn>
2016-07-23 22:07:00 -0700

    Fix previous mistakes to create the global marker variable
    - since marker variable is used everywhere in the file, it is unsafe
    to use it as a global variable and assume its state is at what we
    want at the time of use. Therefore, created searchMarker variable
    to represent it.

--- static/map.js
+++ static/map.js
@@ -15,7 +15,7 @@ var notifiedPokemon = [];
 
 var map;
 var locationMarker;
-var marker;
+var searchMarker;
 
 var light2Style=[{"elementType":"geometry","stylers":[{"hue":"#ff4400"},{"saturation":-68},{"lightness":-4},{"gamma":0.72}]},{"featureType":"road","elementType":"labels.icon"},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"hue":"#0077ff"},{"gamma":3.1}]},{"featureType":"water","stylers":[{"hue":"#00ccff"},{"gamma":0.44},{"saturation":-33}]},{"featureType":"poi.park","stylers":[{"hue":"#44ff00"},{"saturation":-23}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"hue":"#007fff"},{"gamma":0.77},{"saturation":65},{"lightness":99}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"gamma":0.11},{"weight":5.6},{"saturation":99},{"hue":"#0091ff"},{"lightness":-86}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"lightness":-48},{"hue":"#ff5e00"},{"gamma":1.2},{"saturation":-23}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"saturation":-64},{"hue":"#ff9100"},{"lightness":16},{"gamma":0.47},{"weight":2.7}]}];
 var darkStyle=[{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#b39964"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#181818"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"lightness":17},{"color":"#525252"}]}];
@@ -110,8 +110,7 @@ function initMap() {
     map.setMapTypeId(localStorage['map_style']);
     google.maps.event.addListener(map, 'idle', updateMap);
 
-    marker = createSearchMarker();
-
+    createSearchMarker();
     addMyLocationButton();
     initSidebar();
     google.maps.event.addListenerOnce(map, 'idle', function(){
@@ -121,7 +120,7 @@ function initMap() {
 };
 
 function createSearchMarker() {
-    var marker = new google.maps.Marker({
+    searchMarker = new google.maps.Marker({
         position: {
             lat: center_lat,
             lng: center_lng
@@ -132,19 +131,19 @@ function createSearchMarker() {
     });
 
     var oldLocation = null;
-    google.maps.event.addListener(marker, 'dragstart', function() {
-        oldLocation = marker.getPosition();
+    google.maps.event.addListener(searchMarker, 'dragstart', function() {
+        oldLocation = searchMarker.getPosition();
     });
 
-    google.maps.event.addListener(marker, 'dragend', function() {
-        var newLocation = marker.getPosition();
+    google.maps.event.addListener(searchMarker, 'dragend', function() {
+        var newLocation = searchMarker.getPosition();
         changeLocation(newLocation, 
             function() {
                 oldLocation = null;
             },
             function() {
                 if (oldLocation) {
-                    marker.setPosition(oldLocation);
+                    searchMarker.setPosition(oldLocation);
                 }
             }
         );
@@ -153,18 +152,16 @@ function createSearchMarker() {
     // double click event
     google.maps.event.addListener(map, 'dblclick', function(e) {
         var positionDoubleclick = e.latLng;
-        var oldLocation = marker.getPosition();
+        var oldLocation = searchMarker.getPosition();
         changeLocation(positionDoubleclick, 
             function() {
-                marker.setPosition(positionDoubleclick);
+                searchMarker.setPosition(positionDoubleclick);
             },
             function() {
-                marker.setPosition(oldLocation);
+                searchMarker.setPosition(oldLocation);
             }
         );
     });
-
-    return marker;
 }
 
 function initSidebar() {
@@ -187,7 +184,7 @@ function initSidebar() {
         changeLocation(loc, 
             function() {
                 map.setCenter(loc);
-                marker.setPosition(loc);
+                searchMarker.setPosition(loc);
             }
         );
     });
@@ -772,13 +769,13 @@ function myLocationButton() {
                 currentLocation.style.backgroundPosition = '-144px 0px';
 
                 //Change seach position to marker position:
-                var oldLocation = marker.getPosition();
+                var oldLocation = searchMarker.getPosition();
                 changeLocation(latlng,
                     function() {
-                        marker.setPosition(latlng);
+                        searchMarker.setPosition(latlng);
                     },
                     function() {
-                        marker.setPosition(oldLocation);
+                        searchMarker.setPosition(oldLocation);
                     }
                 );
             });
