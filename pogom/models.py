1159d9f30d9695c67909087e70039d50573a615a
Ted Li <tl2493@columbia.edu>
2016-07-24 08:27:44 +0800

    Fixed China coords transformation for pokemon. (#1695)
    * Added back China coords transformation,
    
    which was deleted in some PR.
    
    * Fixed China coords transformation for pokemon.
    
    * Gitignore now working.
    
    * Revert "Gitignore now working."
    
    This reverts commit cd54af9fa8140fee5dac7e1ba9d4bcd40266d6fc.
    
    * Added China coord transformation when pokemons are returned by id.

--- pogom/models.py
+++ pogom/models.py
@@ -75,6 +75,9 @@ class Pokemon(BaseModel):
         pokemons = []
         for p in query:
             p['pokemon_name'] = get_pokemon_name(p['pokemon_id'])
+            if args.china:
+                p['latitude'], p['longitude'] = \
+                    transform_from_wgs_to_gcj(p['latitude'], p['longitude'])
             pokemons.append(p)
 
         return pokemons
@@ -90,6 +93,9 @@ class Pokemon(BaseModel):
         pokemons = []
         for p in query:
             p['pokemon_name'] = get_pokemon_name(p['pokemon_id'])
+            if args.china:
+                p['latitude'], p['longitude'] = \
+                    transform_from_wgs_to_gcj(p['latitude'], p['longitude'])
             pokemons.append(p)
 
         return pokemons
