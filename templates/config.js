a12c270c78601ed3165807e621dd27dabac41764
AJ ONeal <awesome@coolaj86.com>
2016-07-24 03:12:56 -0400

    fix lang autodetect

--- templates/config.js
+++ templates/config.js
@@ -11,7 +11,7 @@ window.CONFIG = {
 window.document.documentElement.lang = '{{lang}}';
 [ 'de', 'en', 'fr', 'pt_br', 'ru', 'zh_cn', 'zh_hk' ].some(function (lang) {
   if (window.navigator.language.match(lang)) {
-    window.document.documentElement.lang = 'en';
+    window.document.documentElement.lang = lang;
     return true;
   }
 });
