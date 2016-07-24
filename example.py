3e06e107b0c313cbcf649711b9294f7ff5310448
JZ6 <jz6jz6jz@gmail.com>
2016-07-23 23:56:51 -0400

    Update example.py

--- example.py
+++ example.py
@@ -1,2 +1,4 @@
 #halpmepl0x
+#for those that messaged me with "i'm on dev, example.py dont work"
+#thunderfox : can we put an example.py in 2.0 that just prints "you're a fucking retard"
 print("Please refer to documentation, there is no more example.py, only runserver.py")
