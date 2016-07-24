a29ab9886ffc28dbaecf32772f936becc3cb0e9c
JZ6 <jz6jz6jz@gmail.com>
2016-07-24 00:07:35 -0400

    gg (#1736)
    * gg
    
    * Update example.py
    
    * Update example.py
    
    * Update example.py

---
+++ example.py
@@ -0,0 +1,8 @@
+#halpmepl0x
+#for those that messaged me with "i'm on dev, example.py dont work"
+#thunderfox : can we put an example.py in 2.0 that just prints "you're a fucking retard"
+#print ("you have been banned lol") lol
+print("Please refer to documentation, there is no more example.py, only runserver.py")
+fuck = raw_input("Would yous till like to message the devs that example.py dont work? ")
+if fuck.lower() != "no":
+  print ("you have been banned lol")
