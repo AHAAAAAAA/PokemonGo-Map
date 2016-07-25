03c0cc13df98cc7f3b5be465939e60a00e093e5f
Dan Pasanen <dan.pasanen@gmail.com>
2016-07-25 15:13:57 -0500

    Fix up this configuration mess (#2021)
    * Kill credentials.json with fire
    * Match up our command line options with config.ini options
    * Switch to ConfigArgParser to make the task of overriding
      config.ini options via command line parameters easier
    * This provides a much easier long-term method of maintaining
      config/argument options

--- Easy Setup/easy-setup.sh
+++ Easy Setup/easy-setup.sh
@@ -19,9 +19,9 @@ echo "Installing required python packages..."
 pip install -r requirements.txt
 
 echo "Configuring Google Maps API..."
-cp ../config/credentials.json.example ../config/credentials.json
+cp ../config/config.ini.example ../config/config.ini
 echo -n "Enter your Google Maps API key here:"
 read key
-sed -i -e "s/\"gmaps_key\"\ :\ \"\"/\"gmaps_key\"\ :\ \""$key"\"/g" ../config/credentials.json
+sed -i -e "s/\"\#gmaps-key\":\ \"\"/\"gmaps-key\":\ \""$key"\"/g" ../config/config.ini
 
 echo "All done!"
