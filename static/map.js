5ea439cdc09134226bb3a2e073b2b5defc1a4f9c
Michał Szewczyk <michal.szewczyk@quantoo.pl>
2016-07-26 13:17:54 +0200

    Don't zoom too close

--- static/map.js
+++ static/map.js
@@ -268,6 +268,15 @@ function initMap() {
         createSearchMarker(locations[idx], idx);
         bounds.extend(new google.maps.LatLng(locations[idx][0], locations[idx][1]));
     }
+
+    // Don't zoom in too far on only one marker
+    if (bounds.getNorthEast().equals(bounds.getSouthWest())) {
+       var extendPoint1 = new google.maps.LatLng(bounds.getNorthEast().lat() + 0.007, bounds.getNorthEast().lng() + 0.007);
+       var extendPoint2 = new google.maps.LatLng(bounds.getNorthEast().lat() - 0.007, bounds.getNorthEast().lng() - 0.007);
+       bounds.extend(extendPoint1);
+       bounds.extend(extendPoint2);
+    }
+
     map.fitBounds(bounds);
 
     addMyLocationButton();
