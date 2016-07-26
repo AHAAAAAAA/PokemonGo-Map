36cf2f674f87c1e7a196df0c9b3d9ad8888b0d4a
hgentry <harrison.c.gentry@gmail.com>
2016-07-26 08:03:14 +0000

    cleanup

--- beehive_builder.sh
+++ beehive_builder.sh
@@ -3,6 +3,6 @@ echo "" > beehive.sh
 while read line || [[ -n "$line" ]]; do
 	echo "python runserver.py -ns -l '$line' -st $1 &" >> beehive.sh
 done < locations.txt
-echo "python runserver.py -os - '$3, $4'" >> beehive.sh
+echo "python runserver.py -os -l '$3, $4'" >> beehive.sh
 chmod u+x beehive.sh
 echo "Beehive generated. Run ./beehive.sh to unleash the swarm."
