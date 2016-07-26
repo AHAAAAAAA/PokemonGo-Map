479d2e2414373c63429ef51cd8f0873fec9ff628
JZ6 <jz6jz6jz@gmail.com>
2016-07-26 19:26:16 -0400

    Disabled Gmaps-key entering via setup.sh
    Hot fix.

--- Tools/Easy-Setup/easy-setup.sh
+++ Tools/Easy-Setup/easy-setup.sh
@@ -18,10 +18,12 @@ sudo python get-pip.py
 echo "Installing required python packages..."
 pip install -r requirements.txt
 
-echo "Configuring Google Maps API..."
-cp ../config/config.ini.example ../config/config.ini
-echo -n "Enter your Google Maps API key here:"
-read key
-sed -i -e "s/\"\#gmaps-key\":\ \"\"/\"gmaps-key\":\ \""$key"\"/g" ../config/config.ini
+#Will fix after moving config file inside Tools Folder.
+
+#echo "Configuring Google Maps API..."
+#cp ../config/config.ini.example ../config/config.ini
+#echo -n "Enter your Google Maps API key here:"
+#read key
+#sed -i -e "s/\"\#gmaps-key\":\ \"\"/\"gmaps-key\":\ \""$key"\"/g" ../config/config.ini
 
 echo "All done!"
