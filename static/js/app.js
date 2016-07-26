d29c4b281e334752f861dd66aa0412558970874f
J.Pagano <jordi.pagano@gmail.com>
2016-07-26 10:33:56 +0200

    Fix nav closing in touch devices when a select2 option is selected

--- static/js/app.js
+++ static/js/app.js
@@ -11,7 +11,8 @@
 	!function(){function t(t){this.el=t;for(var n=t.className.replace(/^\s+|\s+$/g,"").split(/\s+/),i=0;i<n.length;i++)e.call(this,n[i])}function n(t,n,i){Object.defineProperty?Object.defineProperty(t,n,{get:i}):t.__defineGetter__(n,i)}if(!("undefined"==typeof window.Element||"classList"in document.documentElement)){var i=Array.prototype,e=i.push,s=i.splice,o=i.join;t.prototype={add:function(t){this.contains(t)||(e.call(this,t),this.el.className=this.toString())},contains:function(t){return-1!=this.el.className.indexOf(t)},item:function(t){return this[t]||null},remove:function(t){if(this.contains(t)){for(var n=0;n<this.length&&this[n]!=t;n++);s.call(this,n,1),this.el.className=this.toString()}},toString:function(){return o.call(this," ")},toggle:function(t){return this.contains(t)?this.remove(t):this.add(t),this.contains(t)}},window.DOMTokenList=t,n(Element.prototype,"classList",function(){return new t(this)})}}();
 
 	// Vars.
-	var	$body = document.querySelector('body');
+	var	$body = document.querySelector('body'),
+		$mapWrapper = document.querySelector('.map-wrapper');
 
 	// Breakpoints.
 	skel.breakpoints({
@@ -40,8 +41,8 @@
 	});
 
 	// Event: Hide nav on body click/tap.
-	addEventsListener($body, 'click touchend', function(event) {
-		// on ios safari, when navToggle is clicked, 
+	addEventsListener($mapWrapper, 'click touchend', function(event) {
+		// on ios safari, when navToggle is clicked,
 		// this function executes too, so if the target
 		// is the toggle button, exit this function
 		if (event.target.matches('a[href="#nav"]')) { return; }
