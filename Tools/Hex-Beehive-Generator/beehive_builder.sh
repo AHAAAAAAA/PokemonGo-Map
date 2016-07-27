4ea83ecf6e3fdaad65029843640b1d9804378b4f
Autrui <Plop@users.noreply.github.com>
2016-07-27 21:15:34 +0200

    Update beehive_builder.sh

--- Tools/Hex-Beehive-Generator/beehive_builder.sh
+++ Tools/Hex-Beehive-Generator/beehive_builder.sh
@@ -3,6 +3,6 @@ echo "" > beehive.sh
 while read line || [[ -n "$line" ]]; do
 	echo "nohup python ../../runserver.py -ns -l '$line' -st $1 &" >> beehive.sh
 done < locations.txt
-echo "nohup python runserver.py -os -l '$3, $4' &" >> beehive.sh
+echo "nohup python ../../runserver.py -os -l '$3, $4' &" >> beehive.sh
 chmod u+x beehive.sh
 echo "Beehive generated. Run ./beehive.sh to unleash the swarm."
