e2dce69a7feb77093fbca9caacb8f32e87beaf1a
Ahmed Osman <ahmed.osman@nordcloud.com>
2016-07-28 19:11:05 +0200

    Cleaned up Beehive generation files

--- Tools/Hex-Beehive-Generator/beehive_builder.sh
+++ Tools/Hex-Beehive-Generator/beehive_builder.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
+
+#Generate Location data
 python location_generator.py -st $1 -lp $2 -lat $3 -lon $4 > locations.txt
-echo "" > beehive.sh
+
+echo "#!/bin/bash" > beehive.sh
+
+#Generate commands to run workers and add to script
 while read line || [[ -n "$line" ]]; do
 	echo "nohup python ../../runserver.py -ns -l '$line' -st $1 &" >> beehive.sh
 done < locations.txt
+
+#Add command to run front-end with supplied location (the center)
 echo "nohup python ../../runserver.py -os -l '$3, $4' &" >> beehive.sh
+
 chmod u+x beehive.sh
 echo "Beehive generated. Run ./beehive.sh to unleash the swarm."
--- Tools/Hex-Beehive-Generator/location_generator.py
+++ Tools/Hex-Beehive-Generator/location_generator.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import math
 import argparse
 import json
