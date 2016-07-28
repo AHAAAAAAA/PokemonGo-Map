9ef58367540e26ff09039a369de549f3f0b1ce94
Mr-Protocol <Mr-Protocol@users.noreply.github.com>
2016-07-28 19:35:04 -0400

    Added worker delay launch and fixed location
    This will help with low resource servers not kill processes or crash from CPU spike by launching all the workers at once.
    Also applies fixed location flag.

--- Tools/Hex-Beehive-Generator/beehive_builder.sh
+++ Tools/Hex-Beehive-Generator/beehive_builder.sh
@@ -1,8 +1,9 @@
 python location_generator.py -st $1 -lp $2 -lat $3 -lon $4 > locations.txt
 echo "" > beehive.sh
 while read line || [[ -n "$line" ]]; do
-	echo "nohup python ../../runserver.py -ns -l '$line' -st $1 &" >> beehive.sh
+	echo "nohup python ../../runserver.py -ns -l '$line' -st $1 -fl &" >> beehive.sh
+	echo "sleep 30" >> beehive.sh
 done < locations.txt
-echo "nohup python ../../runserver.py -os -l '$3, $4' &" >> beehive.sh
+echo "nohup python ../../runserver.py -os -l '$3, $4' -fl &" >> beehive.sh
 chmod u+x beehive.sh
 echo "Beehive generated. Run ./beehive.sh to unleash the swarm."
