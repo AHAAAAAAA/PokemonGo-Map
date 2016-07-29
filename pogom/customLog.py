951bcf31e93d249b7de66e1930177595a9036d95
justanironic <caleb@penguinbait.com>
2016-07-29 18:02:23 -0400

    Cleaned up code

--- pogom/customLog.py
+++ pogom/customLog.py
@@ -11,17 +11,17 @@ args = get_args()
 #elif args.only:
 #    ONLY = [i.lower().strip() for i in args.only.split(',')]
 
-def pokeData(id,lat,lng,despawntime):
+def pokeData(pokemon,lat,lng,despawntime):
     if args.datatext:
-        pokemon_name = get_pokemon_name(id).lower()
-        pokemon_id = str(id)
+        pokemon_name = get_pokemon_name(pokemon).lower()
         data = True
         if data:
             spawntime = datetime.datetime.now() - datetime.timedelta(seconds=(900 - despawntime))
             desptime = datetime.datetime.now() + datetime.timedelta(seconds=despawntime)
             datafile = args.datatext
             with open(datafile, "a") as myfile:
-                myfile.write("%s,%s,%s,%s,%s,%s,%s\n" % (pokemon_name.encode('utf-8').title(), lat, lng, spawntime.strftime("%H:%M"), spawntime.strftime("%a"), spawntime.strftime("%b %d,%Y"), desptime.strftime("%H:%M")))
+                myfile.write("%s,%s,%s,%s,%s,%s,%s\n" % (pokemon_name.encode('utf-8').title(), lat, lng, spawntime.strftime("%H:%M"),
+                             spawntime.strftime("%a"), spawntime.strftime("%b %d,%Y"), desptime.strftime("%H:%M")))
             def uniquelines(lineslist):
                 unique = {}
                 result = []
