7465e024433e75589ca85216779a0c862bf14b40
Ryan Brideau <ryan.brideau@gmail.com>
2016-07-31 13:40:50 -0300

    Added more data to webhook json (#2795)
    * Added more data to webhook json
    
    * Fixed plus sign literals

--- pogom/models.py
+++ pogom/models.py
@@ -270,7 +270,9 @@ def parse_map(map_dict, iteration_num, step, step_location):
                     'pokemon_id': p['pokemon_data']['pokemon_id'],
                     'latitude': p['latitude'],
                     'longitude': p['longitude'],
-                    'disappear_time': time.mktime(d_t.timetuple())
+                    'disappear_time': time.mktime(d_t.timetuple()),
+                    'last_modified_time': p['last_modified_timestamp_ms'],
+                    'time_until_hidden_ms': p['time_till_hidden_ms']
                 }
 
                 send_to_webhook('pokemon', webhook_data)
