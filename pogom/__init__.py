c0d528727ddde9e7f3a201cadb5b33fbdff88c32
Jeff Minard <jeff@jrm.cc>
2016-07-31 23:23:03 -0700

    Thread (and API) per account provided
     - BREAKING: interactive password prompt removed
     - Removes "thread_delay" as it doesn't really serve a purpose
     - Removed "num_threads" since it's now directly tied to number of accounts
     - `-u/-p/-a` parsing is handled the same, but with better error messages and better interal list handling
     - Removed lat/lon global state from `config`
     - Fixed error where turning off scanning for long periods of time would fail to restart the scanner
     - Removed a bit of dead code
     - search_overseer now creates threads as needed
     - "next location" is no longer hanlded through global config
     - "next location" is handled more gracefully (search_threads no longer aware of it)

--- pogom/__init__.py
+++ pogom/__init__.py
@@ -6,11 +6,8 @@ config = {
     'LOCALES_DIR': 'static/dist/locales',
     'ROOT_PATH': '',
     'DATA_DIR': 'static/dist/data',
-    'ORIGINAL_LATITUDE': None,
-    'ORIGINAL_LONGITUDE': None,
     'GMAPS_KEY': None,
     'REQ_SLEEP': 5,
     'REQ_HEAVY_SLEEP': 30,
-    'REQ_MAX_FAILED': 5,
-    'PASSWORD': None
+    'REQ_MAX_FAILED': 5
 }
