457f8719371166c45be54e924c907faedb8d0770
Chlodochar <Chlodochar@users.noreply.github.com>
2016-08-03 23:00:21 +0200

    checkpoint

--- static/js/app.js
+++ static/js/app.js
@@ -1,62 +1,67 @@
-(function() {
-
-  "use strict";
+;(function () {
+  'use strict'
 
   // Methods/polyfills.
 
   // addEventsListener
-  var addEventsListener = function(o, t, e) {
-    var n, i = t.split(" ");
-    for (n in i) o.addEventListener(i[n], e)
+  var addEventsListener = function (o, t, e) {
+    var n
+    var i = t.split(' ')
+    for (n in i) {
+      o.addEventListener(i[n], e)
+    }
   }
 
   // classList | (c) @remy | github.com/remy/polyfills | rem.mit-license.org
-  ! function() {
-    function t(t) {
-      this.el = t;
-      for (var n = t.className.replace(/^\s+|\s+$/g, "").split(/\s+/), i = 0; i < n.length; i++) e.call(this, n[i])
+  !function () {
+    function t (t) {
+      this.el = t
+      for (var n = t.className.replace(/^\s+|\s+$/g, '').split(/\s+/), i = 0; i < n.length; i++) {
+        e.call(this, n[i])
+      }
     }
 
-    function n(t, n, i) {
+    function n (t, n, i) {
       Object.defineProperty ? Object.defineProperty(t, n, {
         get: i
       }) : t.__defineGetter__(n, i)
     }
-    if (!("undefined" == typeof window.Element || "classList" in document.documentElement)) {
-      var i = Array.prototype,
-        e = i.push,
-        s = i.splice,
-        o = i.join;
+    if (!('undefined' === typeof window.Element || 'classList' in document.documentElement)) {
+      var i = Array.prototype
+      var e = i.push
+      var s = i.splice
+      var o = i.join
       t.prototype = {
-        add: function(t) {
+        add: function (t) {
           this.contains(t) || (e.call(this, t), this.el.className = this.toString())
         },
-        contains: function(t) {
-          return -1 != this.el.className.indexOf(t)
+        contains: function (t) {
+          return -1 !== this.el.className.indexOf(t)
         },
-        item: function(t) {
+        item: function (t) {
           return this[t] || null
         },
-        remove: function(t) {
+        remove: function (t) {
           if (this.contains(t)) {
-            for (var n = 0; n < this.length && this[n] != t; n++);
-            s.call(this, n, 1), this.el.className = this.toString()
+            for (var n = 0; n < this.length && this[n] !== t; n++) {
+              s.call(this, n, 1), this.el.className = this.toString()
+            }
           }
         },
-        toString: function() {
-          return o.call(this, " ")
+        toString: function () {
+          return o.call(this, ' ')
         },
-        toggle: function(t) {
+        toggle: function (t) {
           return this.contains(t) ? this.remove(t) : this.add(t), this.contains(t)
         }
-      }, window.DOMTokenList = t, n(Element.prototype, "classList", function() {
+      }, window.DOMTokenList = t, n(Element.prototype, 'classList', function () {
         return new t(this)
       })
     }
-  }();
+  }()
 
   // Vars.
-  var $body = document.querySelector('body');
+  var $body = document.querySelector('body')
 
   // Breakpoints.
   skel.breakpoints({
@@ -65,110 +70,112 @@
     medium: '(max-width: 980px)',
     small: '(max-width: 736px)',
     xsmall: '(max-width: 480px)'
-  });
+  })
 
   // Disable animations/transitions until everything's loaded.
-  $body.classList.add('is-loading');
+  $body.classList.add('is-loading')
 
-  window.addEventListener('load', function() {
-    $body.classList.remove('is-loading');
-  });
+  window.addEventListener('load', function () {
+    $body.classList.remove('is-loading')
+  })
 
   // Nav.
-  var $nav = document.querySelector('#nav'),
-    $navToggle = document.querySelector('a[href="#nav"]'),
-    $navClose;
+  var $nav = document.querySelector('#nav')
+  var $navToggle = document.querySelector('a[href="#nav"]')
+  var $navClose
 
   // Stats.
-  var $stats = document.querySelector('#stats'),
-    $statsToggle = document.querySelector('a[href="#stats"]'),
-    $statsClose;
+  var $stats = document.querySelector('#stats')
+  var $statsToggle = document.querySelector('a[href="#stats"]')
+  var $statsClose
 
   // Event: Prevent clicks/taps inside the nav from bubbling.
-  addEventsListener($nav, 'click touchend', function(event) {
-    event.stopPropagation();
-  });
+  addEventsListener($nav, 'click touchend', function (event) {
+    event.stopPropagation()
+  })
 
-  if($stats){
+  if ($stats) {
     // Event: Prevent clicks/taps inside the stats from bubbling.
-    addEventsListener($stats, 'click touchend', function(event) {
-      event.stopPropagation();
-    });
+    addEventsListener($stats, 'click touchend', function (event) {
+      event.stopPropagation()
+    })
   }
 
   // Event: Hide nav on body click/tap.
-  addEventsListener($body, 'click touchend', function(event) {
+  addEventsListener($body, 'click touchend', function (event) {
     // on ios safari, when navToggle is clicked,
     // this function executes too, so if the target
     // is the toggle button, exit this function
     if (event.target.matches('a[href="#nav"]')) {
-      return;
+      return
     }
     if ($stats && event.target.matches('a[href="#stats]')) {
-      return;
+      return
     }
-    $nav.classList.remove('visible');
-    if($stats)
-      $stats.classList.remove('visible');
-  });
+    $nav.classList.remove('visible')
+    if ($stats) {
+      $stats.classList.remove('visible')
+    }
+  })
 
   // Toggle.
 
   // Event: Toggle nav on click.
-  $navToggle.addEventListener('click', function(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    $nav.classList.toggle('visible');
-  });
+  $navToggle.addEventListener('click', function (event) {
+    event.preventDefault()
+    event.stopPropagation()
+    $nav.classList.toggle('visible')
+  })
 
   // Event: Toggle stats on click.
-  if($statsToggle){
-    $statsToggle.addEventListener('click', function(event) {
-      event.preventDefault();
-      event.stopPropagation();
-      $stats.classList.toggle('visible');
-    });
+  if ($statsToggle) {
+    $statsToggle.addEventListener('click', function (event) {
+      event.preventDefault()
+      event.stopPropagation()
+      $stats.classList.toggle('visible')
+    })
   }
 
   // Close.
 
   // Create elements.
-  $navClose = document.createElement('a');
-  $navClose.href = '#';
-  $navClose.className = 'close';
-  $navClose.tabIndex = 0;
-  $nav.appendChild($navClose);
+  $navClose = document.createElement('a')
+  $navClose.href = '#'
+  $navClose.className = 'close'
+  $navClose.tabIndex = 0
+  $nav.appendChild($navClose)
 
-  if($stats){
-    $statsClose = document.createElement('a');
-    $statsClose.href = '#';
-    $statsClose.className = 'close';
-    $statsClose.tabIndex = 0;
-    $stats.appendChild($statsClose);
+  if ($stats) {
+    $statsClose = document.createElement('a')
+    $statsClose.href = '#'
+    $statsClose.className = 'close'
+    $statsClose.tabIndex = 0
+    $stats.appendChild($statsClose)
   }
 
   // Event: Hide on ESC.
-  window.addEventListener('keydown', function(event) {
-    if (event.keyCode == 27) {
-      $nav.classList.remove('visible');
-      if($stats)
-        $stats.classList.remove('visible');
+  window.addEventListener('keydown', function (event) {
+    if (event.keyCode === 27) {
+      $nav.classList.remove('visible')
+      if ($stats) {
+        $stats.classList.remove('visible')
+      }
     }
-  });
+  })
 
   // Event: Hide nav on click.
-  $navClose.addEventListener('click', function(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    $nav.classList.remove('visible');
-  });
+  $navClose.addEventListener('click', function (event) {
+    event.preventDefault()
+    event.stopPropagation()
+    $nav.classList.remove('visible')
+  })
 
-  if($statsClose){
+  if ($statsClose) {
     // Event: Hide stats on click.
-    $statsClose.addEventListener('click', function(event) {
-      event.preventDefault();
-      event.stopPropagation();
-      $stats.classList.remove('visible');
-    });
+    $statsClose.addEventListener('click', function (event) {
+      event.preventDefault()
+      event.stopPropagation()
+      $stats.classList.remove('visible')
+    })
   }
-})();
+})()
--- static/js/mobile.js
+++ static/js/mobile.js
@@ -1,64 +1,59 @@
-var useLoc = document.getElementById('use-loc');
-useLoc.checked = localStorage.useLoc === 'true';
-useLoc.onchange = function() {
-  localStorage.useLoc = useLoc.checked;
-};
+var useLoc = document.getElementById('use-loc')
+useLoc.checked = localStorage.useLoc === 'true'
+useLoc.onchange = function () {
+  localStorage.useLoc = useLoc.checked
+}
 
-
-var navBtn = document.querySelector('#nav button');
-navBtn.onclick = function() {
+var navBtn = document.querySelector('#nav button')
+navBtn.onclick = function () {
   if (localStorage.useLoc !== 'true') {
-    navBtn.disabled = true;
-    return (location.href="mobile");
-  }
-  else if ("geolocation" in navigator) {
+    navBtn.disabled = true
+    return (location.href = 'mobile')
+  } else if ('geolocation' in navigator) {
     // Getting the GPS position can be very slow on some devices
-    navBtn.disabled = true;
-    navBtn.innerText = 'Locating...';
+    navBtn.disabled = true
+    navBtn.innerText = 'Locating...'
 
     // Get location and use it!
-    navigator.geolocation.getCurrentPosition(function(p) {
-      navBtn.innerText = 'Reloading...';
-      location.href='mobile?lat='+p.coords.latitude+'&lon='+p.coords.longitude;
-
-    }, function(err) {
-      navBtn.innerText = 'Reload';
-      navBtn.disabled = false;
-      alert('Failed to get location: ' + err.message);
-
+    navigator.geolocation.getCurrentPosition(function (p) {
+      navBtn.innerText = 'Reloading...'
+      location.href = 'mobile?lat=' + p.coords.latitude + '&lon=' + p.coords.longitude
+    }, function (err) {
+      navBtn.innerText = 'Reload'
+      navBtn.disabled = false
+      alert('Failed to get location: ' + err.message)
     }, {
       enableHighAccuracy: true,
       timeout: 5000,
       maximumAge: 5000
-    });
+    })
+  } else {
+    alert('Your device does not support web geolocation')
   }
-  else {
-    alert('Your device does not support web geolocation');
-  }
-};
+}
 
-function updateTimes() {
+function updateTimes () {
   // server tells us how many seconds are left we note the
   // pageload time and count down from there.
   // Yes, this could be a smidge innaccurate, but not by
   // more than 1 second or so which doesn't matter.
   // And now we don't have to deal with timestamps and dates!
-  var remains = document.querySelectorAll('div.remain');
+  var remains = document.querySelectorAll('div.remain')
   for (var i = 0; i < remains.length; ++i) {
-    var element = remains[i];
-    var now = new Date().getTime();
-    var secondsPassed = Math.floor((now - page_loaded)/1000);
-    var alivefor =  element.getAttribute('disappear');
-    var remain = alivefor - secondsPassed;
-    var min = Math.floor(remain / 60);
-    var sec = remain % 60;
-    element.innerText = (remain > 0) ? min + ' min ' + sec + ' sec' : '(expired)';
+    var element = remains[i]
+    var now = new Date().getTime()
+    var secondsPassed = Math.floor((now - pageLoaded) / 1000)
+    var alivefor = element.getAttribute('disappear')
+    var remain = alivefor - secondsPassed
+    var min = Math.floor(remain / 60)
+    var sec = remain % 60
+    element.innerText = (remain > 0) ? min + ' min ' + sec + ' sec' : '(expired)'
   }
-};
-setInterval(updateTimes, 1000);
+}
+setInterval(updateTimes, 1000)
 
-document.querySelectorAll("li").forEach(function(listItem) {
-  listItem.onclick = function() {
-    window.document.location = this.getAttribute('href');
-  };
-});
+document.querySelectorAll('li').forEach(function (listItem) {
+  listItem.onclick = function () {
+    window.document.location = this.getAttribute('href')
+  }
+})
