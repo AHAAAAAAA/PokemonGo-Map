d5b24f8fd80fe095c19094cef083c60c7c0750e8
Karol Lassak <ingwar@ingwar.eu.org>
2016-08-03 11:10:45 +0200

    Clean scanned locations (#2888)
    * Added cleaning Scanned Locations table
    
    * Changed classmethod to static method

--- pogom/models.py
+++ pogom/models.py
@@ -75,8 +75,8 @@ class Pokemon(BaseModel):
     class Meta:
         indexes = ((('latitude', 'longitude'), False),)
 
-    @classmethod
-    def get_active(cls, swLat, swLng, neLat, neLng):
+    @staticmethod
+    def get_active(swLat, swLng, neLat, neLng):
         if swLat is None or swLng is None or neLat is None or neLng is None:
             query = (Pokemon
                      .select()
@@ -104,8 +104,8 @@ class Pokemon(BaseModel):
 
         return pokemons
 
-    @classmethod
-    def get_active_by_id(cls, ids, swLat, swLng, neLat, neLng):
+    @staticmethod
+    def get_active_by_id(ids, swLat, swLng, neLat, neLng):
         if swLat is None or swLng is None or neLat is None or neLng is None:
             query = (Pokemon
                      .select()
@@ -194,8 +194,8 @@ class Pokestop(BaseModel):
     class Meta:
         indexes = ((('latitude', 'longitude'), False),)
 
-    @classmethod
-    def get_stops(cls, swLat, swLng, neLat, neLng):
+    @staticmethod
+    def get_stops(swLat, swLng, neLat, neLng):
         if swLat is None or swLng is None or neLat is None or neLng is None:
             query = (Pokestop
                      .select()
@@ -237,8 +237,8 @@ class Gym(BaseModel):
     class Meta:
         indexes = ((('latitude', 'longitude'), False),)
 
-    @classmethod
-    def get_gyms(cls, swLat, swLng, neLat, neLng):
+    @staticmethod
+    def get_gyms(swLat, swLng, neLat, neLng):
         if swLat is None or swLng is None or neLat is None or neLng is None:
             query = (Gym
                      .select()
@@ -268,8 +268,8 @@ class ScannedLocation(BaseModel):
     class Meta:
         indexes = ((('latitude', 'longitude'), False),)
 
-    @classmethod
-    def get_recent(cls, swLat, swLng, neLat, neLng):
+    @staticmethod
+    def get_recent(swLat, swLng, neLat, neLng):
         query = (ScannedLocation
                  .select()
                  .where((ScannedLocation.last_modified >=
@@ -389,6 +389,16 @@ def parse_map(map_dict, step_location):
     
     return True
 
+    clean_database()
+
+def clean_database():
+    flaskDb.connect_db()
+    query = (ScannedLocation
+            .delete()
+            .where((ScannedLocation.last_modified <
+                (datetime.utcnow() - timedelta(minutes=30)))))
+    query.execute()
+    flaskDb.close_db(None)
 
 
 def bulk_upsert(cls, data):
