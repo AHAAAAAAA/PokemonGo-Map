e46e42c0da0e38f63db20bf19df8b0b06ec84e76
FrostTheFox <frost@walnutgaming.com>
2016-08-28 02:20:37 -0400

    Add files via upload

---
+++ example.py
@@ -0,0 +1,16 @@
+# halpmepl0x
+# for those that messaged me with "i'm on dev, example.py dont work"
+# thunderfox : can we put an example.py in 2.0 that just prints "you're a fucking retard"
+# invisiblek wuz here
+# balthemel wuz also 'ere
+
+# dont make pr to fix pls
+
+print("Please refer to documentation, there is no more example.py, only runserver.py")
+fuck = raw_input("Would yous till like to massage the devs that example.py no work?")
+if "needful" in fuck.lower():
+    print("poon bang lure pls")
+elif fuck.lower() != "no":
+    print("you have been banned lol")
+else:
+    print("bless u")
